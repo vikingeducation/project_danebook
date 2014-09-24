58ade172ba2f0e37b774484a87fb936e0bbb176e
Michael Alexander <alexandermw@gmail.com>
2014-09-24 14:26:27 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/friends' => 'static_pages#friends'
   get '/photos' => 'static_pages#photos'
   get '/about' => 'static_pages#about'
+  get '/about_edit' => 'static_pages#about_edit'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

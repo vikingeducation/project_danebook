2e12d5f48c3b4ab24776a61c6410f561178f2c98
Michael Alexander <alexandermw@gmail.com>
2014-09-25 11:25:00 -0700

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def timeline
     @friends = [ "Ron<br>Weasley", "Hermione<br>Granger", "Ginny<br>Weasley", 
                  "paragraph<br>text", "paragraph<br>text", "paragraph<br>text"]
+    NUM_FRIENDS = 6
   end
   
 end

91c6206ffc6974ea8cc6ff338e901965496e4004
A. Sivasailam <cldfusion@gmail.com>
2014-09-26 01:38:56 -0400

    rails app

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def index
+  end
+
+  def photos
+  end
+
+  def friends
+  end
+
+  def timeline
+  end
+
+  def about
+  end
+
+  def edit
+  end
+end

72a80c25d5f5b9cdedd12e2c27d696b479b08ef5
Michael Alexander <alexandermw@gmail.com>
2014-09-25 19:00:24 -0700

    refactoring

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,6 @@ class StaticPagesController < ApplicationController
   def timeline
     @friends = [ "Ron<br>Weasley", "Hermione<br>Granger", "Ginny<br>Weasley", 
                  "paragraph<br>text", "paragraph<br>text", "paragraph<br>text"]
-    NUM_FRIENDS = 6
   end
   
 end

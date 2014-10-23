cfce1af03719dd770bba362cfc06334c733b9455
Andur Carr <carr.andur@gmail.com>
2014-10-22 20:24:31 -0700

    Add remember me to sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
-      sign_in(@user)
+      if params[:remember]
+        permanent_sign_in(@user)
+      else
+        sign_in(@user)
+      end
       flash[:success] = "Signed in"
       redirect_to @user
     else

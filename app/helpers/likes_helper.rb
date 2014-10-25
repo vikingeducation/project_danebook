883d3fb79265c3d77e71c5c7aeaffc45689f3b57
Andur Carr <carr.andur@gmail.com>
2014-10-25 01:14:25 -0700

    Add like count helper

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -6,4 +6,15 @@ module LikesHelper
       link_to "Like", [object, :likes], method: "POST"
     end
   end
+
+  def likes_count(object)
+    scale = object.likes.size
+    string = false if scale == 0
+    string = "#{object.likes[0].user.name} " if scale > 0
+    string += "#{object.likes[1].user.name} " if scale > 1
+    string += "#{pluralize(object.likes_count, 'other person')} " if scale > 2
+    like = (scale > 1 ? "like" : "likes")
+
+    string ? "<div class='col-xs-12'><p class='pull-left'>#{string + like + ' this.'}</p></div>".html_safe : nil
+  end
 end

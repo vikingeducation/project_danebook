3c4708f6ff5ee35b1a7ff302a0b29502026250f1
Andur Carr <carr.andur@gmail.com>
2014-10-25 12:13:16 -0700

    Build comment form and display comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.includes(:author, :likes)
+    @posts = @user.posts.includes(:author, :likes, :comments)
     @post = current_user.posts.build
   end
 

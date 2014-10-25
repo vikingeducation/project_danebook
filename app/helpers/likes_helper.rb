04ee783097d6fef2fb5d3fa85b08e50e2aa9e5ca
Andur Carr <carr.andur@gmail.com>
2014-10-25 00:41:45 -0700

    Add helper for like button

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,9 @@
 module LikesHelper
+  def like_button(object)
+    if object.likes.find_by_user_id(current_user.id)
+      link_to "Unlike", [object, object.likes.find_by_user_id(current_user.id)], method: "DELETE"
+    else
+      link_to "Like", [object, :likes], method: "POST"
+    end
+  end
 end

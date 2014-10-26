ebb76fca4057f5d57bd3ddd132ed24a714c46cf8
Tom McLaughlin <tim5046@gmail.com>
2014-10-26 15:13:09 -0400

    tweak

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,8 +39,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @hometown = Location.get_location(@user.location_id).to_s
-    @current = Location.get_location(@user.current_location_id).to_s
+    if @user.location_id
+      @hometown = Location.get_location(@user.location_id).to_s
+    end
+    if @user.current_location_id
+      @current = Location.get_location(@user.current_location_id).to_s
+    end
   end
 
   private

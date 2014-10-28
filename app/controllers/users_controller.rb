9035372ee2175f8bcff9e0dd172391864fa1162d
Michael Alexander <alexandermw@gmail.com>
2014-10-27 19:06:53 -0700

    remove bangsave on user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(whitelisted_params)
-    if @user.save!
+    if @user.save
       sign_in(@user)
       flash[:success] = "Welcome to Danebook!"
       redirect_to @user

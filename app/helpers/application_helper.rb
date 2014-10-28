38e61cc146707e180c248d968bcb8b2d2701ea12
Andur Carr <carr.andur@gmail.com>
2014-10-27 21:57:32 -0700

    Rebuild hero

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def interactive_navbar
+    if signed_in?
+      render 'shared/search'
+    else
+      render 'shared/sign_in'
+    end
+  end
 end
--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,6 +1,8 @@
 module FriendsHelper
   def friend_button(target)
-    if current_user.friends.include?(target)
+    if current_user == target
+      nil
+    elsif current_user.friends.include?(target)
       link_to "Unfriend", friend_path(target), method: "DELETE", data: { confirm: "Are you sure you want to unfriend #{target.name}?"}, class: "btn btn-danish"
     elsif current_user.friended_users.include?(target)
       link_to "Withdraw Request", friend_path(target), method: "DELETE", class: "btn btn-danish"

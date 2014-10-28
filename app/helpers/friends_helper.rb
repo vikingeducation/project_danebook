09225042aad26f13b35606242e5733529720bf10
Andur Carr <carr.andur@gmail.com>
2014-10-27 23:02:49 -0700

    Cosmetic improvements

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,15 +1,15 @@
 module FriendsHelper
-  def friend_button(target)
+  def friend_button(target, css_class=nil)
     if current_user == target
       nil
     elsif current_user.friends.include?(target)
-      link_to "Unfriend", friend_path(target), method: "DELETE", data: { confirm: "Are you sure you want to unfriend #{target.name}?"}, class: "btn btn-danish"
+      link_to "Unfriend", friend_path(target), method: "DELETE", data: { confirm: "Are you sure you want to unfriend #{target.name}?"}, class: css_class
     elsif current_user.friended_users.include?(target)
-      link_to "Withdraw Request", friend_path(target), method: "DELETE", class: "btn btn-danish"
+      link_to "Withdraw Request", friend_path(target), method: "DELETE", class: css_class
     elsif current_user.users_friended_by.include?(target)
-      link_to "Accept Friend Request", user_friends_path(target), method: "POST", class: "btn btn-danish"
+      link_to "Accept Friend Request", user_friends_path(target), method: "POST", class: css_class
     else
-      link_to "Send Friend Request", user_friends_path(target), method: "POST", class: "btn btn-danish"
+      link_to "Send Friend Request", user_friends_path(target), method: "POST", class: css_class
     end
   end
 end

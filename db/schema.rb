48e0dd4ac4b8749d26a066cf593b6f71cc64ff74
Michael Alexander <alexandermw@gmail.com>
2014-11-02 09:17:45 -0800

    working with foreman

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141031045238) do
+ActiveRecord::Schema.define(version: 20141102170202) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -21,6 +21,22 @@ ActiveRecord::Schema.define(version: 20141031045238) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "delayed_jobs", force: true do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "friendings", force: true do |t|
     t.integer  "friender_id"

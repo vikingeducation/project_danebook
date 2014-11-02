940adec76a8601bb0ff033cfe75c2bd072249fe9
Michael Alexander <alexandermw@gmail.com>
2014-11-01 21:37:06 -0700

    add actionmailer config settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,15 @@ Rails.application.configure do
       :secret_access_key => Rails.application.secrets.aws_secret_access_key
     }
   }
+
+  config.action_mailer.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
+    }
+    config.action_mailer.delivery_method ||= :smtp
 end

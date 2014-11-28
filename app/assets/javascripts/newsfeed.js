0a23e20cbc489b035bcb95879393a535dd3d936a
Michael Alexander <alexandermw@gmail.com>
2014-11-28 09:11:26 -0800

    fiddling

--- app/assets/javascripts/newsfeed.js
+++ app/assets/javascripts/newsfeed.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   //when on main newsfeed page
   if ($('#post-feed').length){
 
-    $('#post-feed').on('click', '.single-post .comment-link', 
+    $('#post-feed').on('click', '.comment-link', 
         function(event){
           $parentPost = $(event.target).parents('.single-post')
           $parentPost.find('.comment-form').slideToggle('fast');

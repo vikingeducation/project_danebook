9c7a79ad1a7fd168511412daf611a26e26551f01
Michael Alexander <alexandermw@gmail.com>
2014-11-30 20:47:51 -0800

    validation tweaks

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,10 +5,10 @@ class Profile < ActiveRecord::Base
   validates :day, :month, :year, :gender,
                                 presence: true
 
-  validates :gender, inclusion: { in: ["Male", "Female", "Another Identity"] }
+  validates :gender, inclusion: { in: ["Male", "Female", "Another Identity", "Not Provided"] }
   validates :day, inclusion: { in: (1..31) }
   validates :month, inclusion: { in: (1..12) }
-  validates :year, inclusion: { in: (1905..Time.now.year) }
+  validates :year, inclusion: { in: (1901..Time.now.year) }
 
   validates :college, length: { maximum: 25 }
   validates :hometown, length: { maximum: 35 }

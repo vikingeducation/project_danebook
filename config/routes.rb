7674b06e570cab7385370c187b2e7e0d487a11db
Dan Berger <dsberger@gmail.com>
2015-02-20 12:07:20 -0800

    renamed path from index to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'static_pages#index'
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

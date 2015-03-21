5d3cb28e50864a1ee9654651fdbcbb6fa5d554f8
Michael Alexander <alexandermw@gmail.com>
2015-03-20 18:53:58 -0700

    remove  error in comment tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,17 +3,4 @@ require 'rails_helper'
 describe Comment do
   let (:comment){ FactoryGirl.create(:post_comment)}
 
-  it 'is valid if it has a body and a user' do
-    expect(comment).to be_valid
-  end
-
-  it 'is not valid if body is empty' do
-    expect(FactoryGirl.build(:post, body: nil)).not_to be_valid
-  end
-
-  it 'is not valid if it does not have a parent user' do
-    expect(FactoryGirl.build(:post, user_id: nil)).not_to be_valid
-  end
-
-
 end

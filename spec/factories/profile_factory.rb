793509a6392fe5671d7e8bc4fbabd896405b97c8
Schwad <nicholas.schwaderer@gmail.com>
2015-03-30 01:06:59 +0000

    initialize the spec files for models

--- spec/factories/profile_factory.rb
+++ spec/factories/profile_factory.rb
@@ -2,13 +2,13 @@ FactoryGirl.define do
 
   factory :profile do
     profile_owner
-    :college "U of M"
-    :birthday "9/9/99"
-    :hometown "Missoula"
-    :currenttown "Helena"
-    :telephone "23432432"
-    :words "hi im bob"
-    :aboutme "djkflas;jfkl;dsajkfdl;s"
+    college "U of M"
+    birthday "9/9/99"
+    hometown "Missoula"
+    currenttown "Helena"
+    telephone "23432432"
+    words "hi im bob"
+    aboutme "djkflas;jfkl;dsajkfdl;s"
   end
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,5 @@
+
+describe User do
+  
+
+end

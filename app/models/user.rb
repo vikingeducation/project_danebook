29acbc97cf6a10bceced2d99a2cdc3d17236b993
Schwad <nicholas.schwaderer@gmail.com>
2015-04-06 18:19:30 +0000

    change associations can now post everywhere

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_one :profile, dependent: :destroy
-  has_many :posts, dependent: :destroy
+  has_many :received_posts, :class_name => "Post", :foreign_key => :receiver_id, dependent: :destroy
+  has_many :authored_posts, :class_name => "Post", :foreign_key => :user_id, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  accepts_nested_attributes_for :profile, :posts, :likes
+  accepts_nested_attributes_for :profile, :received_posts, :authored_posts, :likes
   validates :email, :uniqueness => true
   validates :password,
             :length => {:within => 6..25},

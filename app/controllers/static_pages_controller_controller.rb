04ec6ae2f538d220752942de7ee95db3981602a5
ayva <olga.ayva@gmail.com>
2015-07-20 10:53:35 -0700

    Links changed

--- app/controllers/static_pages_controller_controller.rb
+++ app/controllers/static_pages_controller_controller.rb
@@ -5,4 +5,6 @@ class StaticPagesControllerController < ApplicationController
 
   
 
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,17 @@ Rails.application.routes.draw do
   
   root "static_pages_controller#index"
 
-  get 'about', to: 'static_pages_controller#about'
+  get 'about', to: 'static_pages_controller#about', as: 'about'
 
-  get 'about_edit', to: 'static_pages_controller#about_edit'
+  get 'about_edit', to: 'static_pages_controller#about_edit', as: 'about_edit'
 
-  get 'friends', to: 'static_pages_controller#friends'
+  get 'friends', to: 'static_pages_controller#friends', as: 'friends'
 
-  get 'photos', to: 'static_pages_controller#photos'
+  get 'photos', to: 'static_pages_controller#photos', as: 'photos'
 
-  get 'timeline', to: 'static_pages_controller#timeline'
+  get 'timeline', to: 'static_pages_controller#timeline', as: 'timeline'
+
+
 
 
 end

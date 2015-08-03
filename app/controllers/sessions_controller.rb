82052bfc92e317ee533d3ee5834753112adf8559
David <davidmeza@outlook.com>
2015-08-03 18:48:03 -0400

    add user token authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
-      sign_in(@user)
+      params[:remember_me] ? permanent_sign_in(@user) : sign_in(@user)
       flash[:success] = "You've successfully signed in"
       redirect_to current_user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  # ----------------------- Validations --------------------
+
   validates :first_name, :last_name, :email,
             :length => { :in => 1..30 },
             :presence => true
@@ -13,8 +15,24 @@ class User < ActiveRecord::Base
             :length => { :in => 8..24 },
             :allow_nil => true
 
+  before_create :generate_token
+
+  # ----------------------- Methods --------------------
+
   def name
     "#{first_name} #{last_name}"
+  end
+
+  def generate_token
+    begin
+      self[:auth_token] = SecureRandom.urlsafe_base64
+    end while User.exists?(:auth_token => self[:auth_token])
+  end
+
+  def regenerate_auth_token
+    self.auth_token = nil
+    generate_token
+    save!
   end
 
 end

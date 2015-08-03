21b1bf74ac797e4a2aa9352f327b84de4ad7e783
Alice Fung <alice.o.fung@gmail.com>
2015-08-03 18:19:17 -0400

    fix migration to only require email and password

--- db/migrate/20150803213800_create_users.rb
+++ db/migrate/20150803213800_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
 
-      t.string :first_name,       null: false
-      t.string :last_name,        null: false
+      t.string :first_name
+      t.string :last_name
       t.string :email,            null: false
       t.string :password_digest,  null: false
       t.date   :birthdate

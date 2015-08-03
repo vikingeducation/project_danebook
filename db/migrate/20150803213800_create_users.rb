624d610c2e2c066465e7b3a5fc2cae91f4cabd67
Alice Fung <alice.o.fung@gmail.com>
2015-08-03 18:04:13 -0400

    user migration file indexed

--- db/migrate/20150803213800_create_users.rb
+++ db/migrate/20150803213800_create_users.rb
@@ -2,10 +2,15 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
 
-      t.string :email, null:false
-      t.string :password_digest, null:false
+      t.string :first_name,       null: false
+      t.string :last_name,        null: false
+      t.string :email,            null: false
+      t.string :password_digest,  null: false
+      t.date   :birthdate
+      t.string :sex
 
       t.timestamps null: false
     end
+    add_index :users, [:email], unique: :true
   end
 end

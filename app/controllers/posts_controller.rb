33d809c488fe66a6a3ec5bc00980e62a72ac2e36
shadefinale <shadefinale@gmail.com>
2015-08-04 13:03:12 -0500

    About section on timeline uses actual users information rather than the mockup. Can properly delete posts. Checks that the user is authorized to post/delete posts on timeline.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
+  before_action :find_user
+  before_action :require_current_user, only: [:create, :destroy]
+
   def index
-    @user = User.find(params[:user_id])
     @posts = @user.written_posts
     @new_post = Post.new(author: @user)
   end
@@ -9,14 +11,37 @@ class PostsController < ApplicationController
     @post = Post.new(whitelist_post_params)
     if @post.save
       flash[:success] = "Successfully posted to your timeline!"
-      redirect_to user_posts_path
+      redirect_to user_posts_path(current_user)
     else
       flash[:notice] = "Couldn't post to your timeline..."
-      redirect_to user_posts_path
+      redirect_to user_posts_path(current_user)
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:success] = "Post deleted Successfully!"
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:notice] = "Couldn't delete post, try again."
+      redirect_to user_posts_path(current_user)
+    end
+  end
+
+
   private
+
+    def require_current_user
+      unless current_user == @user
+        flash[:notice] = "You cannot perform this action as a different user."
+        redirect_to user_posts_path(@user)
+      end
+    end
+
+    def find_user
+      @user = User.includes(:profile).find(params[:user_id])
+    end
 
     def whitelist_post_params
       params.require(:post).permit(:user_id, :body)
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,9 @@
 module PostsHelper
+  def profile_helper(field)
+    if field && !field.blank?
+      field
+    else
+      "This user has not yet added this information!"
+    end
+  end
 end

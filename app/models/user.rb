a637967496e9f1d83124aea6e373a8bb81af1836
Alice Fung <alice.o.fung@gmail.com>
2015-08-05 18:46:10 -0400

    revert user match_like method to params instead of obj

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,8 +26,8 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :profile, :reject_if => :all_blank
 
 
-  def match_like(obj)
-    likes.where("likings_id = ? AND likings_type = ?", obj.id, obj.class.to_s).first
+  def match_like(params)
+    likes.where("likings_id = ? AND likings_type = ?", params[:likings_id], params[:likings_type]).first
     # fail
   end
 

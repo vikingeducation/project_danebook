11d75750fcbf2d436c0391431d9aabc3883e8d46
shadefinale <shadefinale@gmail.com>
2015-08-05 16:51:25 -0500

    Improve display of users liking likable count

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -17,9 +17,31 @@ module PostsHelper
 
   def present_likes_count(likable)
     if current_user && current_user.likes?(likable)
-      return "You and #{pluralize(likable.likes.count - 1, 'other person like', 'other people like')} this!"
+      return "You#{friend_string(likable)} #{other_people_like_string(likable)}like this!"
     else
-      return "#{likable.likes.count} people like this!"
+      return "#{pluralize(likable.likes.count, 'person likes', 'people like')} this!"
     end
+  end
+
+  def other_people_like_string(likable)
+    "and #{pluralize(likable.likes.count - 1, 'other person ', 'other people ')} " if likable.likes.count > 1
+  end
+
+  def friend_string(likable)
+    if current_user.friends.any?{|friend| friend.likes?(likable)}
+      generate_friend_string(likable)
+    else
+      return ""
+    end
+  end
+
+  def generate_friend_string(likable)
+    return_string = ", "
+    friends_that_like = current_user.friends.select{|friend| friend.likes?(likable)}
+    friends_that_like.each do |friend|
+      return_string += link_to friend.full_name, friend
+      return_string += ", "
+    end
+    return return_string
   end
 end

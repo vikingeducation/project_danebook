dd9384f472177e6c5a545dbce5ce6b8c656fc0ab
Adam <adam.kinson@gmail.com>
2015-08-05 18:53:46 -0400

    finish New Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,46 @@
 class PostsController < ApplicationController
 
+  before_action :require_current_user, :except => [:index]
+
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
+    @new_post = Post.new(:author_id => current_user.id)
   end
 
 
   def new
   end
+
+
+  def create
+    @new_post = Post.new(post_params)
+    if @new_post.save
+      flash[:success] = "New post created!"
+      redirect_to [current_user, :posts]
+    else
+      flash.now[:danger] = "New post not saved. Please try again."
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+      render :index
+    end
+  end
+
+
+  private
+
+
+    def post_params
+      params.require(:post).permit(:author_id, :body)
+    end
+
+
+    def require_current_user
+      unless params[:user_id] == current_user.id.to_s
+        flash[:danger] = "You're not authorized to do this!"
+        redirect_to user_posts_path(params[:user_id])
+      end
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :users, :only => [:new, :create, :show, :edit, :update] do
     resource :profile, :only => [:new]
-    resources :posts, :only => [:new, :index]
+    resources :posts, :only => [:new, :create, :index]
   end
 
   get "/about" => "users#show"

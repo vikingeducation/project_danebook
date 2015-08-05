fc667312b5e1abaa44a318bc8d44385ab49bcf2c
Joseph Lozano <lozanojoseph@gmail.com>
2015-08-05 15:14:02 -0700

    added comment controller and model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
+
   belongs_to :profile
   has_many :likes, as: :likeable, dependent: :destroy
-
+  has_many :comments, dependent: :destroy
+  
   def already_liked_by?(current_user)
     return false unless current_user
     self.likes.where(:user_id => current_user.id).count > 0
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805171303) do
+ActiveRecord::Schema.define(version: 20150805215918) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body",       default: "", null: false
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer  "user_id"

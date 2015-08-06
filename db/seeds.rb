0cd1c12238e730a10fc989d2d438bb81bf09829a
ayva <olga.ayva@gmail.com>
2015-08-05 17:43:20 -0700

    heroku bug fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# MULT=30
+#   create_table "profiles", force: :cascade do |t|
+#     t.integer  "user_id"
+#     t.string   "email"
+#     t.string   "college"
+#     t.string   "phone"
+#     t.string   "hometown"
+#     t.string   "homecountry"
+#     t.string   "livesintown"
+#     t.string   "livesincountry"
+#     t.string   "wordsby"
+#     t.string   "wordsabout"
+#     t.datetime "created_at",     null: false
+#     t.datetime "updated_at",     null: false
+#   end
+# MULT.times do 
+#   User.create()
+#   create_table "users", force: :cascade do |t|
+#     t.string   "email"
+#     t.string   "first_name"
+#     t.string   "last_name"
+#     t.date     "birthday"
+#     t.string   "gender"
+#     t.datetime "created_at",      null: false
+#     t.datetime "updated_at",      null: false
+#     t.string   "password_digest"
+#   end

49a3ed3f39ab55cc62824bd2175ccffafda86d4a
Joseph Lozano <lozanojoseph@gmail.com>
2015-08-06 13:56:33 -0700

    wrote user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,31 @@ describe User do
     expect(user).to be_valid
   end
 
-  it "should have a profile" do
-    expect{user.profile}.to raise_error
+  it "should have a profile after creation" do
+    user.save
+    expect(user.profile.id).not_to be_nil
   end
-  
+
+  it "should have likes association" do
+    expect{user.likes}.to_not raise_error
+  end
+
+  it "should have comments association" do
+    expect{user.comments}.to_not raise_error
+  end
+
+  it "should have an auth_token on save" do
+    user.save
+    expect(user.auth_token).not_to be_nil    
+  end
+
+  it "should generate a new token on regenerate_token" do
+    user.save
+    token = user.auth_token
+    user.regenerate_token
+    regen_token = user.auth_token
+    expect(token).not_to eql(regen_token)
+  end
+
   
 end

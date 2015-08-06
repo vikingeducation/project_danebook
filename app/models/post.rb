cc6e54d98d359f0ced2504727262e44d40cb7b3e
Adam <adam.kinson@gmail.com>
2015-08-06 19:24:15 -0400

    finish rendering of basic Post on Timeline

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 
-  belongs_to :user, :foreign_key => :author_id
+  belongs_to :author, :class_name => 'User', :foreign_key => :author_id
 
   validates :body, :author_id, :presence => :true
   validates :body, :length => { in: 1..255 }

3f3a376ac30d5efb93a5423134145cbd22b05e79
Adam Kinson <adam.kinson@gmail.com>
2015-08-07 16:10:31 -0400

    fix comment Like styling

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,29 +1,23 @@
 module LikesHelper
 
 
-  def display_likers(post)
-    names = get_liker_names(post)
-
-    name_string = names.join(" and ")
-    verb = conjugate_like(names)
-
-    if post.likers.count > 2
-      "#{name_string} and #{pluralize(post.likers.count - 2, 'other')} #{verb} this."
-    elsif post.likers.count > 0
-       "#{name_string} #{verb} this."
+  def display_likers(object)
+    if object.class.to_s == 'Comment'
+      comment_display(object)
+    else
+      post_display(object)
     end
-
   end
 
 
   private
 
 
-    def get_liker_names(post)
+    def get_liker_names(object)
       names = []
-      names << "You" if liked_by_current_user?(post)
+      names << "You" if liked_by_current_user?(object)
 
-      post.likers.first(2).map do |liker|
+      object.likers.first(2).map do |liker|
         names << liker.profile.full_name unless liker == current_user
       end
 
@@ -31,8 +25,8 @@ module LikesHelper
     end
 
 
-    def liked_by_current_user?(post)
-      current_user && post.likers.include?(current_user)
+    def liked_by_current_user?(object)
+      current_user && object.likers.include?(current_user)
     end
 
 
@@ -42,6 +36,32 @@ module LikesHelper
       else
         'like'
       end
+    end
+
+
+    def post_display(object)
+      names = get_liker_names(object)
+
+      name_string = names.join(" and ")
+      verb = conjugate_like(names)
+
+      if object.likers.count > 2
+        "#{name_string} and #{pluralize(object.likers.count - 2, 'other')} #{verb} this."
+      elsif object.likers.count > 0
+         "#{name_string} #{verb} this."
+      end
+    end
+
+
+    def comment_display(object)
+      count = object.likers.count
+
+      if count > 1 && liked_by_current_user?(object)
+        "You and #{pluralize(count - 1, 'other')} like this."
+      elsif count > 1 && !liked_by_current_user?(object)
+        "#{pluralize(count - 1, 'other')} like this."
+      end
+
     end
 
 end

afb2bba077a2780c69817e68b0866a6fb44e0d8f
shadefinale <shadefinale@gmail.com>
2015-08-08 16:56:21 -0500

    Finished post spec and added presence validation to commentables to avoid empty comments/posts.

--- app/models/concerns/commentable.rb
+++ app/models/concerns/commentable.rb
@@ -5,6 +5,7 @@ module Commentable
     has_many :likes, as: :likable, dependent: :destroy
     has_many :people_who_like, through: :likes, source: :user
     belongs_to :author, class_name: "User", foreign_key: :user_id
+    validates :body, presence: true
   end
 
   def posted_on

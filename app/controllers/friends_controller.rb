c596a54b3f7d8656a0fc6ee9b030b7790fa65700
facingsouth <xin.d.liu@gmail.com>
2015-08-08 14:46:15 -0700

    cleanup

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,6 +4,7 @@ class FriendsController < ApplicationController
     @user = User.find(params[:user_id]) if params[:user_id]
     @user ||= current_user
     @friends = @user.friended_users
+                    .includes(:friended_users)
                     .includes(:profile)
   end
 

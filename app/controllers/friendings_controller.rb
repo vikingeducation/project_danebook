cc0322ffe6fe5a71c2776a7c7a9f7d43425345e8
Alice Fung <alice.o.fung@gmail.com>
2015-08-10 18:17:50 -0400

    add friending destroy action

--- app/controllers/friendings_controller.rb
+++ app/controllers/friendings_controller.rb
@@ -1,19 +1,37 @@
 class FriendingsController < ApplicationController
 
-  # before_action :require_current_users
+  before_action :require_login
 
   def create
-    current_user = User.find(params[:current_user_id])
-    friending_recipient = User.find(params[:id])
+    friending_recipient = User.find(params[:user_id])
 
     if current_user.friended_users << friending_recipient
       flash[:success] = "You friended #{friending_recipient.name}"
       redirect_to friending_recipient
-
     else
       flash[:error] = "Friending failure. Try again!"
       redirect_to friending_recipient
     end
   end
+
+   def destroy
+    #can also delete friending from friending join table directly
+    #using association below
+    unfriended_user = User.find(params[:user_id])
+    if current_user.friended_users.delete(unfriended_user)
+      flash[:success] = "Unfriended #{unfriended_user.full_name}!"
+      redirect_to current_user
+    else
+      flash[:error] = "Unfriending failure. Try again!"
+      redirect_to unfriended_user #not unfriended yet
+    end
+  end
+
+  private
+
+  def params_list
+      params.require(:friending).permit(:id, :user_id)
+  end
+
 
 end

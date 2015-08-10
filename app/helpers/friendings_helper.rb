f1cfd0151c36cd8df79984d3783719788dbc89f8
Alice Fung <alice.o.fung@gmail.com>
2015-08-10 19:58:58 -0400

    friends display

--- app/helpers/friendings_helper.rb
+++ app/helpers/friendings_helper.rb
@@ -1,16 +1,16 @@
 module FriendingsHelper
 
-  def friend_unfriend_button
+  def friend_unfriend_button(friend_id = params[:user_id])
     friended = current_user.friended_users.
-                            find_by_id(params[:user_id])
+                            find_by_id(friend_id)
 
     # #if friended by, still considered friend by button (not in db)
     # friended ||= current_user.users_friended_by.
-    #                           find_by_id(params[:user_id])
+    #                           find_by_id(friend_id)
     if friended
-      link_to "Remove Friend", friending_path(friended.id, :friend_id => params[:user_id]), method: :delete, class: "btn btn-info btn-lg", id: "friending"
+      link_to "Remove Friend", friending_path(friended.id, :friend_id => friend_id), method: :delete, class: "btn btn-info btn-lg", id: "friending"
     else
-      link_to "Friend Me", friendings_path(:friend_id => params[:user_id]), method: :post, class: "btn btn-info btn-lg", id: "friending"
+      link_to "Friend Me", friendings_path(:friend_id => friend_id), method: :post, class: "btn btn-info btn-lg", id: "friending"
     end
   end
 end

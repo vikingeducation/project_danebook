9049c4f868967dcbac32ed05b28327ef870a7fc2
Alice Fung <alice.o.fung@gmail.com>
2015-08-10 20:06:18 -0400

    num display on navbar

--- app/helpers/friendings_helper.rb
+++ app/helpers/friendings_helper.rb
@@ -13,4 +13,9 @@ module FriendingsHelper
       link_to "Friend Me", friendings_path(:friend_id => friend_id), method: :post, class: "btn btn-info btn-lg", id: "friending"
     end
   end
+
+  def friend_num
+    num = User.find(params[:user_id]).friends.size
+    num > 0 ? "(#{num.to_s})"  : ""
+  end
 end

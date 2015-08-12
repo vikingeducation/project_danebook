52915d8b295c4949dc1ab3f016b03831a3257c3d
Joseph Lozano <lozanojoseph@gmail.com>
2015-08-12 15:38:07 -0700

    changed aws hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
     :s3_credentials => {
       # put your host name here if needed
       # see the reading below for more details
-      :s3_host_name => "s3-us-east-1.amazonaws.com",
+      :s3_host_name => "s3-us-west-2.amazonaws.com",
       # NOTE: these lines are changed to use secrets.yml
       # from the examples (which use ENV vars instead)
       :bucket => Rails.application.secrets.s3_bucket_name,

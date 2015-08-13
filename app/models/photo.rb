467c16fb069084e6f064bb9357115cc8663b93bf
Joseph Lozano <lozanojoseph@gmail.com>
2015-08-13 10:42:28 -0700

    photo size validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ActiveRecord::Base
   has_attached_file :picture, :styles => { :medium => "350x350>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+   validates_attachment_size :picture, :less_than => 2.megabytes
 
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :comments, as: :commentable, dependent: :destroy

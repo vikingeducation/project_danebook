247f7543b181e55845702490210d2340b6d4539b
alokp <alokpra@gmail.com>
2015-08-13 15:53:43 -0700

    profile and cover photo in views

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,16 +1,16 @@
 module PhotosHelper
 
-  def display_cover_photo
-    if object_owner.profile.cover_photo
-      image_tag(object_owner.profile.cover_photo.image.url, class: 'img-responsive cover-photo hidden-xs hidden-sm').html_safe
+  def display_cover_photo(owner=object_owner)
+    if owner.profile.cover_photo
+      image_tag(owner.profile.cover_photo.image.expiring_url(10), class: 'img-responsive cover-photo hidden-xs hidden-sm').html_safe
     else
       asset_path 'blank_cover_photo.jpg'
     end
   end
 
-  def display_profile_photo
-    if object_owner.profile.cover_photo
-      image_tag(object_owner.profile.profile_photo.image.url, class: 'img-responsive hidden-xs hidden-sm')
+  def display_profile_photo(owner=object_owner)
+    if owner.profile.cover_photo
+      image_tag(owner.profile.profile_photo.image.expiring_url(10), class: 'img-responsive hidden-xs hidden-sm')
     else
       asset_path 'blank_profile_photo.jpg'
     end

8853f976240d1fc1cb054722fbfba5ed65fe85a5
Adam <adam.kinson@gmail.com>
2015-08-14 20:53:09 -0400

    finish User controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,9 +3,40 @@ require 'rails_helper'
 
 describe UsersController do
 
-  describe 'POST #update' do
+  describe 'PATCH #update' do
 
-    it 'rejects when user_id in params is not current_user'
+    let(:initiator) { create(:user) }
+    let(:victim) { create(:user) }
+
+    before do
+      initiator
+      request.cookies[:auth_token] = initiator.auth_token
+    end
+
+
+    it 'allows a user to edit his own profile' do
+      legit_update = 'Fancy University'
+      put :update,  :id => initiator.id,
+                    :user => attributes_for(:user,
+                             :profile_attributes => { :college => legit_update, :id => initiator.profile.id } )
+
+      initiator.reload
+      expect(initiator.profile.college).to eq(legit_update)
+      expect(flash[:success]).to eq('Profile updated!')
+    end
+
+
+    it 'rejects when user_id in params is not current_user' do
+      malicious_update = 'Loser College'
+      put :update,  :id => victim.id,
+                    :user => attributes_for(:user,
+                             :profile_attributes => { :college => malicious_update, :id => victim.profile.id } )
+
+      victim.reload
+      expect(victim.profile.college).not_to eq(malicious_update)
+      expect(flash[:danger]).to eq("You're not authorized to do this!")
+    end
+
 
   end
 

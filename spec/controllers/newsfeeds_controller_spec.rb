34b3dc7ab40c1bb5f539c657727d73d1e59d6943
Adam <adam.kinson@gmail.com>
2015-08-28 08:11:10 -0400

    build test for Newsfeed before_action

--- spec/controllers/newsfeeds_controller_spec.rb
+++ spec/controllers/newsfeeds_controller_spec.rb
@@ -5,30 +5,54 @@ RSpec.describe NewsfeedsController, type: :controller do
 
   describe 'GET #show' do
 
-    let(:user) { create(:user) }
-    let(:friend) { create(:user) }
+    context "current user's newsfeed" do
 
-    before do
-      request.cookies[:auth_token] = user.auth_token
-      user.friended_users << friend
-      get :show, :user_id => user.id
+      let(:user) { create(:user) }
+      let(:friend) { create(:user) }
+
+      before do
+        request.cookies[:auth_token] = user.auth_token
+        user.friended_users << friend
+        get :show, :user_id => user.id
+      end
+
+
+      it { should use_before_action(:require_current_user) }
+
+      it 'assigns @user to current user' do
+        expect(assigns[:user]).to eq(user)
+      end
+
+      it 'collects latest posts from friends into @posts' do
+        posts = create_list(:post, 2, :poster_id => friend.id)
+
+        # dummy post that should not be pulled
+        create(:post)
+
+        expect(assigns[:posts].size).to eq(2)
+        expect(posts).to include(assigns[:posts].first)
+      end
+
+      it { should render_template('show') }
+
     end
 
 
-    it { should use_before_action(:require_current_user) }
+    context "other user's newsfeed" do
 
-    it 'assigns @user to current user' do
-      expect(assigns[:user]).to eq(user)
-    end
+      let(:user) { create(:user) }
+      let(:other_user) { create(:user) }
 
-    it 'collects latest posts from friends into @posts' do
-      posts = create_list(:post, 2, :poster_id => friend.id)
+      before do
+        request.cookies[:auth_token] = user.auth_token
+      end
 
-      # dummy post that should not be pulled
-      create(:post)
+      it "prevents viewing another user's feed" do
+        get :show, :user_id => other_user.id
+        expect(flash[:danger]).to eq("You're not authorized to do this!")
+        expect(response).to redirect_to(root_path)
+      end
 
-      expect(assigns[:posts].size).to eq(2)
-      expect(posts).to include(assigns[:posts].first)
     end
 
 

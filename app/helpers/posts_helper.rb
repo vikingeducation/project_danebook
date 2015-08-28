113d60c063b9453ff10eba06e29e82dc8563278e
Adam Kinson <adam.kinson@gmail.com>
2015-08-28 14:46:31 -0400

    added delete confirmation for posts & comments

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+
+  def post_index_user_field(label, field)
+    unless field.nil? || field.empty?
+      "<p>
+        <strong>#{label}: </strong>
+        #{field}
+      </p>".html_safe
+    end
+  end
+
+
 end

5a2376cb0fcbc98621556099844d457726a11985
Adam <adam.kinson@gmail.com>
2015-08-29 10:26:53 -0400

    add better error styling around forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def render_error(resource, field_sym=nil)
-    "<small>#{field_sym.to_s.titleize} #{resource.errors[field_sym].first}</small>".html_safe unless resource.errors[field_sym].empty?
+    "<small class='error-message'>#{field_sym.to_s.titleize} #{resource.errors[field_sym].first}</small>".html_safe unless resource.errors[field_sym].empty?
   end
 
   def user_image(user)

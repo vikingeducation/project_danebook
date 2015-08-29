957afce852de931096f7bd4a858378bd593be57d
Adam <adam.kinson@gmail.com>
2015-08-29 17:37:51 -0400

    refactor navbar logic into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,18 @@
 module ApplicationHelper
 
+  def render_navbar
+    if signed_in_user?
+      render :partial => 'shared/navbar_user'
+    else
+      render :partial => 'shared/navbar_guest'
+    end
+  end
+
+
   def render_error(resource, field_sym=nil)
     "<small class='error-message'>#{field_sym.to_s.titleize} #{resource.errors[field_sym].first}</small>".html_safe unless resource.errors[field_sym].empty?
   end
+
 
   def user_image(user)
     if user.profile_photo
@@ -11,6 +21,7 @@ module ApplicationHelper
       asset_path 'profile_photo_default.png'
     end
   end
+
 
   def cover_image(user)
     if user.cover_photo

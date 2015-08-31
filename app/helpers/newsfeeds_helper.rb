22c61fff389a5db9446200743114ec2bcd26a454
Adam Kinson <adam.kinson@gmail.com>
2015-08-31 17:54:16 -0400

    implement helper method for empty posts or recently active friend lists

--- app/helpers/newsfeeds_helper.rb
+++ app/helpers/newsfeeds_helper.rb
@@ -1,2 +1,12 @@
 module NewsfeedsHelper
+
+  def render_recently_active(collection, message_if_empty)
+    if collection.size ==0
+      "<h5 class='text-center text-muted'><em>#{message_if_empty}</em></h5>".html_safe
+    else
+      render :partial => 'friend', :collection => collection
+    end
+  end
+
+
 end

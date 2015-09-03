5c4a972b8281c653d24b8aeaf10e1b9123794b37
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-03 10:59:45 -0700

    switched from robohash b/c its down

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   has_attached_file :profile_pic, styles: {small: '150x150'}
   validates_attachment_content_type :profile_pic, :content_type => /\Aimage\/.*\Z/
-  has_attached_file :cover_pic, styles: {large: '1080x400'}, default_url: 'https://lh4.googleusercontent.com/2xuY-di5SzGyHX4sdPdfyX0IJVF-1T2Ea1__e5DYbNaIao8TisOf0nBgIJNT3Y6ZfMawOP1jI_R2y-xr4y7EMyHr70s-U1yZsemmDjn16irK5Q9ALK4BjKLEZ4z36Pc-XF0kWcg' 
+  has_attached_file :cover_pic, styles: {large: '1080x400'}, default_url: 'https://lh4.googleusercontent.com/2xuY-di5SzGyHX4sdPdfyX0IJVF-1T2Ea1__e5DYbNaIao8TisOf0nBgIJNT3Y6ZfMawOP1jI_R2y-xr4y7EMyHr70s-U1yZsemmDjn16irK5Q9ALK4BjKLEZ4z36Pc-XF0kWcg'
   validates_attachment_content_type :cover_pic, :content_type => /\Aimage\/.*\Z/
 
   def email
@@ -16,12 +16,11 @@ class Profile < ActiveRecord::Base
 
   def avatar_url
     if self.profile_pic.url != "/profile_pics/original/missing.png"
-      self.profile_pic.url 
+      self.profile_pic.url
     else
-      "https://robohash.org/#{self.user_id}"
+      "http://flathash.com/#{self.user_id}" # switch b/c robohash is down
     end
   end
-
 
   def missing_url
     "/profile_pics/original/missing.png"

7bf5cf0d3e7c871505ce7a0c79bb27c964ef471c
facingsouth <xin.d.liu@gmail.com>
2015-09-10 09:03:12 -0700

    add birthday validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,11 @@
 class Profile < ActiveRecord::Base
   # validates :user_id, presence: true
   validates :first_name, :last_name, 
-            :birthday_month, :birthday_day, :birthday_year, 
+            :birthday_month, :birthday_day, :birthday_year, :birthday,
             :gender, 
             :presence => true
+
+  validate  :validate_birthday_within
 
   validates :hometown, 
             :current_lives, 
@@ -19,8 +21,20 @@ class Profile < ActiveRecord::Base
 
   belongs_to :user
 
+#----------------  Methods  ---------------
+
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+#----------------  Validator  --------------- 
+
+  def validate_birthday_within
+    if self.birthday < (Time.now.to_date - 100*365)
+      self.errors.add(:birthday, "Sorry, our site is too dangerous for a 100-year-old man!")
+    elsif self.birthday > (Time.now.to_date - 12*365)
+      self.errors.add(:birthday, "Sorry, you must be at least 12 to use our site!")
+    end
   end
 
 
@@ -32,8 +46,9 @@ class Profile < ActiveRecord::Base
   end
 
   def birthday=(date)
-    self.birthday_year = date[1]
-    self.birthday_month = date[2]
-    self.birthday_day = date[3]
+    fields = date.split('-')
+    self.birthday_year = fields[0].to_i
+    self.birthday_month = fields[1].to_i
+    self.birthday_day = fields[2].to_i
   end
 end

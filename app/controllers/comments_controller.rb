df188f68d8efacc20f0181d277d6c23217a2c9d8
Joseph Lozano <lozanojoseph@gmail.com>
2015-09-10 14:31:55 -0700

    ajaxed likes posts comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,18 +5,23 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     @comment.save
-    @post = Post.find(@comment.commentable_id)
 
     respond_to do |format|
-      format.js {render template: 'profiles/new_comment.js.erb'}
+      format.js {render template: 'profiles/comments.js.erb'}
       format.html {redirect_to request.referrer}
     end
+
   end
 
   def destroy
-    comment = Comment.find_by_id(params[:id])
-    comment.destroy if comment
-    redirect_to request.referrer
+    @comment = Comment.find_by_id(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+      format.js {render template: 'profiles/comments.js.erb'}
+      format.html {redirect_to request.referrer}
+    end
+
   end
 
   private

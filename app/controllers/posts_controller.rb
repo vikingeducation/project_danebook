7b325c7a96773a2db3ab2ae07c1ebb32eb555778
Alice Fung <alice.o.fung@gmail.com>
2015-09-10 21:47:17 -0400

    ajaxed forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,21 +24,21 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params_list)
     @post.user_id = current_user.id
+    respond_to do |format|
     if @post.save
       flash[:success] = "You created a post"
+      format.js
     else
       flash[:error] = "There was an error creating your post."
+      format.js { render template: 'shared/flash_display' }
     end
 
-    respond_to do |format|
-        format.html { redirect_to user_posts_path(current_user) }
-        format.js { render template: 'shared/flash_display' }
+      format.html { redirect_to user_posts_path(current_user) }
     end
   end
 
   def destroy
     @post = Post.find(params[:id])
-    # @thing = @post
     respond_to do |format|
       if (current_user.id == @post.user_id) && @post.destroy
         flash[:success] = "Your post has been deleted!"

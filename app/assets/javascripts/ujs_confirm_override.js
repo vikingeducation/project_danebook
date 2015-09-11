dfe4c0ee6060b18694b3722d8c659677674f7141
shadefinale <shadefinale@gmail.com>
2015-09-10 19:37:03 -0500

    Refactor confirm dialog override and move it into its own file, ujs_confirm_override.js

---
+++ app/assets/javascripts/ujs_confirm_override.js
@@ -0,0 +1,28 @@
+// Override the default behavior of showing an alert
+// We could refactor this into smaller functions,
+// but I want to avoid polluting any other name spaces.
+$.rails.allowAction = function(link) {
+  var previousDataConfirm,
+      previousText;
+
+  // If the link does not have data-confirm, we don't need to do anything.
+  if (!link.attr('data-confirm')) return true;
+
+  // Store the previous values of our link in case they don't confirm.
+  previousDataConfirm = link.attr('data-confirm');
+  previousText = link.text();
+
+  // Modify our link to remove data-confirm so the next time it's clicked
+  // we just execute. Edit the text to be whatever was passed into confirm:
+  link.removeAttr('data-confirm');
+  link.text(previousDataConfirm);
+
+  // After however long, just set our link back to before we clicked it.
+  setTimeout(function(){
+    link.off("click")
+    link.attr('data-confirm', previousDataConfirm);
+    link.text(previousText);
+  }, 2000)
+
+  return false;
+};

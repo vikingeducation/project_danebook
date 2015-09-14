900a018ff1bbae889d5e43c4520e827fb9329a11
facingsouth <xin.d.liu@gmail.com>
2015-09-13 20:44:19 -0700

    fix bug(show comment form button will only show the corresponding comment form now)

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -5,13 +5,16 @@ $(document).ready(function(){
 })
 
 function slideCommentFormListener() {
-  $(document).on('click', '.show-comment-form', function(){
-    slideCommentForm();
+  $(document).on('click', '.show-comment-form', function(e){
+    e.preventDefault();
+    var $target = $(e.target);
+    slideCommentForm($target);
   });
 }
 
-function slideCommentForm() {
-  var $form = $('.comment-form')
+function slideCommentForm($target) {
+  var dataID = $target.data('id');
+  var $form = $(".comment-form[data-id='" + dataID + "']");
   if ($form.css('display') == 'none') {
     $form.slideDown(300);
   } else {

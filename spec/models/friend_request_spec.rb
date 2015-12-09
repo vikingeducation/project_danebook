870e681ad42d7e3309a097705ec0081c644acb0b
Bideo Wego <bideowego@gmail.com>
2015-12-09 15:36:47 -0500

    Remove unused model concern adapter_acknowledgeable

--- spec/models/friend_request_spec.rb
+++ spec/models/friend_request_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 describe FriendRequest do
   describe '#initiator' do
+    it 'returns the user who initiated the friend request'
   end
 
   describe '#approver' do
+    it 'returns the user who approves the friend request'
   end
 
   describe '#accept' do
+    it 'triggers a after update callback'
+    it 'results in a friendship being created when set to true'
+    it 'results in no friendship being created if set to false'
   end
 end
 
+# TODO
+#   test Dateable
+#   test Friendable
+#   test Notifiable

2757dc08d2d393e780bde1675671f58543f76731
Julia Herron <julia.herron@gmail.com>
2016-02-12 13:47:22 -0800

    i have figured out all my problems! (for now)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
 
   def index
-    @posts = Post.where(user_id: params[:user_id])
+    @posts = Post.where(user_id: params[:user_id]).order("created_at DESC")
     @new_post = current_user.posts.build if signed_in_user?
   end
 

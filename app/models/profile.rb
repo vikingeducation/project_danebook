d025a22aefcd2e849eaa0ffc32facc95c85c0893
Andrew Baik <and.baik@gmail.com>
2016-02-11 22:53:58 -0500

    fixed birthday validation on profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,6 @@ class Profile < ActiveRecord::Base
   belongs_to :user, inverse_of: :profile
 
   validates :gender, inclusion: { in: %w(Male Female) }
-  validates :birth_day, inclusion: { in: 1..31 }
-  validates :birth_month, inclusion: { in: 1..12 }
-  validates :birth_year, inclusion: { in: 1900..2016 }
   validates :college, length: { maximum: 36 }
   validates :from, length: { maximum: 36 }
   validates :lives, length: { maximum: 36 }

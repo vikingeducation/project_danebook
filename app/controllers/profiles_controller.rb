14b6678a4544eecaa659e642889f51cafd07f7a2
Julia Herron <julia.herron@gmail.com>
2016-02-11 16:54:29 -0800

    built out profile table

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,22 +1,34 @@
 class ProfilesController < ApplicationController
   layout "profile"
 
-  def edit
-  end
-
-
-
-  def update
-  end
 
 
   def show
+    @profile = Profile.find_by_user_id(params[:user_id])
+  end
+
+  def edit
+    @profile = Profile.find_by_user_id(params[:user_id])
+  end
+
+
+  def update
+    @profile = find_by_user_id(params[:user_id])
+    if @profile.update(whitelisted_params)
+      flash[:success] = "Updates saved"
+      redirect_to user_profile_path(@profile.user)
+    else
+      flash[:error] = "Unable to save profile"
+      render :edit
+    end
   end
 
 
 
-
-
+  private
+  def whitelisted_params
+    params.require(:profile).permit(:user_id)
+  end
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,48 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Profile.delete_all
 User.delete_all
 
 puts "Creating users..."
 
+
+def create_user
+  new_user = User.create(
+    first_name: Faker::Name.first_name, 
+    last_name: Faker::Name.last_name, 
+    email: Faker::Internet.email, 
+    password_digest: "$2a$10$.MSTCUKj.7tpap8LswJXa.AeTlsa9Qmh0TWTcYJjqZd8bhMokgPbO", 
+    birthday: Time.now, 
+    gender: ["Male", "Female"].sample
+  )
+end
+
 10.times do
-  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password_digest: "$2a$10$.MSTCUKj.7tpap8LswJXa.AeTlsa9Qmh0TWTcYJjqZd8bhMokgPbO", birthday: Time.now, gender: ["Male", "Female"].sample)
+  create_user
+end
+
+Profile.all.each do |p|
+  p.about_me = Faker::Hipster.sentence
+  p.words_to_live_by = Faker::Hipster.paragraph
+  p.hometown = Faker::Address.city
+  p.current_city = Faker::Address.city
+  p.save
 end
 
 
-User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "admin@admin.com", password_digest: "$2a$10$.MSTCUKj.7tpap8LswJXa.AeTlsa9Qmh0TWTcYJjqZd8bhMokgPbO", birthday: Time.now, gender: "Female")
+admin = User.create(
+  first_name: Faker::Name.first_name, 
+  last_name: Faker::Name.last_name, 
+  email: "admin@admin.com", 
+  password_digest: "$2a$10$.MSTCUKj.7tpap8LswJXa.AeTlsa9Qmh0TWTcYJjqZd8bhMokgPbO", 
+  birthday: Time.now, 
+  gender: "Female"
+  )
+
+admin_profile = Profile.find_by_user_id(admin.id)
+admin_profile.about_me = Faker::Hipster.sentence
+admin_profile.words_to_live_by = Faker::Hipster.paragraph
+admin_profile.hometown = Faker::Address.city
+admin_profile.current_city = Faker::Address.city
+admin_profile.save

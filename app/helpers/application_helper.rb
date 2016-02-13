d0f24ba8b5d405c2f596c99d6462b5399ef51322
Julia Herron <julia.herron@gmail.com>
2016-02-12 17:37:56 -0800

    moved error msgs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
 
 
   def field_with_errors(object, field)
+
     unless object.errors[field].empty?
       content_tag(:div, class: "bg-danger") do
         "#{field.to_s.titleize} #{object.errors[field].first}"

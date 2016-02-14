ff874c47ff8f98193a06ca71073fb519d04e03fe
Julia Herron <julia.herron@gmail.com>
2016-02-14 11:33:42 -0800

    another try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   config.cache_classes = true
   config.serve_static_assets = true
   config.assets.compile = true
-  config.assets.digest = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
@@ -32,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.

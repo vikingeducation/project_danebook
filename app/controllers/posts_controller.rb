6a198d08b34856c58fb47ad64f95d49c8da545f0
Andrew Baik <and.baik@gmail.com>
2016-02-15 04:09:41 -0500

    switched from nested form to regular way for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,31 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:create, :destroy]
 
-  # def create
-  #   @post = Post.new(post_params)
-  #   if @post.save
-  #
-  #   else
-  #
-  #   end
-  # end
-  #
-  # def create
-  #   @post = Post.new(post_params)
-  #   if @post.save
-  #     flash[:success] = "You've created a post!"
-  #     redirect_to timeline_path
-  #   else
-  #     flash[:danger] = "Failed to create a post!"
-  #     redirect_to :back
-  #   end
-  # end
-  #
-  # def destroy
-  # end
-  #
-  # private
-  #
-  # def post_params
-  #   params.require(:post).permit(:body, :user_id)
-  # end
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id if current_user
+    if @post.save
+      flash[:success] = "You've created a post!"
+    else
+      flash[:danger] = "Failed to create a post!"
+    end
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:success] = "You've deleted a post!"
+      redirect_to user_path(current_user)
+    else
+      flash[:danger] = "Failed to delete a post!"
+
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
+  end
 end

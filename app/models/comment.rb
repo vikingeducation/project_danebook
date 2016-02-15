de9949b5155d8810ce5f7368beb9311ff874b360
Andrew Baik <and.baik@gmail.com>
2016-02-15 15:57:02 -0500

    polymorphic associations for like-comment setup

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
+  has_many :likes, as: :likeable, dependent: :destroy
 
   validates :body, presence: true, length: { maximum: 500 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   resource :session, only: [:create, :destroy]
   resources :posts, only: [:create, :destroy] do
     resources :likes, only: [:create, :destroy], defaults: { likeable: 'Post' }
-  end 
+    resources :comments, only: [:create, :destroy], defaults: { commentable: 'Post'} do
+      resources :likes, only: [:create, :destroy], defaults: { likeable: 'Comment' }
+    end
+  end
   root 'users#new', as: :signup
 
 end

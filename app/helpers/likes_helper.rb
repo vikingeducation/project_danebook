203d24a48bd2d20164a324929935ad07040d1b87
Andrew Baik <and.baik@gmail.com>
2016-02-15 14:09:19 -0500

    fixed like statistics helper method

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -4,28 +4,49 @@ module LikesHelper
   def display_like_stats(post)
     post_likes = post.likes
     size = post_likes.size
-    liked = post_likes.any? { |l| l.user_id == current_user.id } ? true : false
-    result = ""
-    if liked
-      result += "You and "
-      post_likes = post_likes.select { |l| l.user_id != current_user.id }
-    end
-    puts result
-    if size < 3
-      post_likes.each_with_index do |l, index|
-        user = User.find(l.user_id)
-        if index == size - 1
-          result += link_to(user.name, user_path(user))
-        else
-          result += link_to(user.name, user_path(user)) + " and "
-        end
-      end
+    if signed_in_user?
+      liked = post_likes.any? { |l| l.user_id == current_user.id } ? true : false
     else
-      remaining = liked ? sized - 3 : size - 2
-      result += "and #{remaining} others"
+      liked = false
     end
-      result += " likes this."
-      return result
+    result = ""
+    result += "You and " if liked
+
+    remaining = liked ? size - 3: size - 2
+
+    post_likes = post_likes.select { |l| l.user_id != current_user.id } if signed_in_user?
+
+    post_likes.each_with_index do |like, index|
+      user = User.find(like.user_id)
+
+      if index == 1 || index == size - 2
+        result += link_to(user.name, user_path(user))
+        break
+      elsif index < 1
+        result += link_to(user.name, user_path(user)) + " and "
+      end
+    end
+
+    result += " and #{remaining} others" if remaining > 0
+
+    result += " likes this."
+    return result
+    # else
+    #   liked = false
+    #   result = ""
+    #     post_likes.each_with_index do |l, index|
+    #       user = User.find(l.user_id)
+    #       if index <= 2
+    #         result += link_to(user.name, user_path(user))
+    #       else
+    #         result += link_to(user.name, user_path(user)) + " and "
+    #       end
+    #     end
+    #     remaining = liked ? size - 3: size - 2
+    #     result += " and #{remaining} others"
+    #   result += " likes this."
+    #   return result
+    # end
   end
 
 end

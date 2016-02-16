1479efb4a5269cee7f25139bcfc2039e4078b564
Julia Herron <julia.herron@gmail.com>
2016-02-16 15:08:46 -0800

    tiny friends box on timeline

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,4 +18,15 @@ module ApplicationHelper
   end 
 
 
+
+  def all_friends_count(user)
+    user.friended_users.count + user.users_friended_by.count
+  end
+
+
+  def all_friends(user)
+    user.friended_users + user.users_friended_by
+  end
+
+
 end
--- app/helpers/friendings_helper.rb
+++ app/helpers/friendings_helper.rb
@@ -1,14 +1,2 @@
 module FriendingsHelper
-
-  def all_friends_count(user)
-    user.friended_users.count + user.users_friended_by.count
-  end
-
-
-  def all_friends(user)
-    user.friended_users + user.users_friended_by
-  end
-
-
-
 end

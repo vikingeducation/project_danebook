0e2207e9127c09c69b5b36e5ec4bad2a64fc8754
Andrew Baik <and.baik@gmail.com>
2016-02-21 19:20:11 -0500

    finished friending basic controller specs

--- app/controllers/friendings_controller.rb
+++ app/controllers/friendings_controller.rb
@@ -3,8 +3,9 @@ class FriendingsController < ApplicationController
   before_action :require_login, only: [:create, :destroy]
 
   def create
-    friending_recipient = User.find(params[:id])
-    if current_user.friendeds << friending_recipient
+    friending_recipient = User.find_by_id(params[:id])
+    @friending = Friending.new(friender: current_user, friended: friending_recipient)
+    if @friending.save
       flash[:success] = "Successfully friended #{friending_recipient.name}"
       redirect_to friending_recipient
     else
@@ -14,10 +15,11 @@ class FriendingsController < ApplicationController
   end
 
   def destroy
-    unfriended_user = User.find(params[:id])
-    if current_user.friendeds.delete(unfriended_user)
+    friending_recipient = User.find_by_id(params[:id])
+    @friending = Friending.find_by_friender_id_and_friended_id(current_user.id, friending_recipient.id)
+    if @friending && @friending.destroy
       flash[:success] = "Successfully unfriended"
-      redirect_to current_user
+      redirect_to :back
     else
       flash[:danger] = "Failed to unfriend"
       redirect_to current_user

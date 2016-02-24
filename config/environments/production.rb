685448ae36b5a31d659038a0043dce429d08715f
Deepa Kackar <dkackar99@yahoo.com>
2016-02-24 11:35:25 -0800

    Changed the handling of bucket name in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
       # NOTE: these lines are changed to use secrets.yml
       # from the examples (which use ENV vars instead)
-      :bucket => Rails.application.secrets.s3_bucket_name,
+      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => Rails.application.secrets.aws_access_key_id,
       :secret_access_key => Rails.application.secrets.aws_secret_access_key
     }

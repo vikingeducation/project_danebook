1d193e0b17e7c58e97bec0c91bc821be2155d499
Thomas Lo <thomasjinlo@gmail.com>
2016-02-24 10:06:16 -0800

    Change foreign key in photos table

--- db/migrate/20160224031110_addattach.rb
+++ db/migrate/20160224031110_addattach.rb
@@ -1,6 +1,6 @@
 class Addattach < ActiveRecord::Migration
   def change
     add_attachment :photos, :image
-    add_column :photos, :user_id, :integer
+    add_column :photos, :author_id, :integer
   end
 end

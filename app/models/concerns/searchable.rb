7df1fa80dba444290143bd32f5d3101a56723728
Julia Herron <julia.herron@gmail.com>
2016-02-24 15:16:10 -0800

    removed some buttons to friend yourself

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -5,11 +5,11 @@ module Searchable
     def search(query)
       if query && query.include?(" ")
         queries = query.split(" ")
-        where("first_name LIKE ?", "%#{queries[0]}%")
-        where("last_name LIKE ?", "%#{queries[1]}%")
+        where("first_name ILIKE ?", "%#{queries[0]}%")
+        where("last_name ILIKE ?", "%#{queries[1]}%")
       elsif query
-        where("first_name LIKE ?", "%#{query}%")
-        where("last_name LIKE ?", "%#{query}%")
+        where("first_name ILIKE ?", "%#{query}%")
+        where("last_name ILIKE ?", "%#{query}%")
       else
         where("")
       end

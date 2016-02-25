c1fe2f7f584758a6af49b1d3a7f54c37d96ace07
Thomas Lo <thomasjinlo@gmail.com>
2016-02-24 16:37:27 -0800

    allow nil for update

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user, inverse_of: :profile
   
-  validates :birthday, :gender, presence: true
+  validates :birthday, :gender, presence: true,
+            :allow_nil => true
   validates :user, presence: true
 end

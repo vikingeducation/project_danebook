7ca98f1864303914f8c81b43ce4e00e54e54ea0b
Thomas Lo <thomasjinlo@gmail.com>
2016-02-24 16:57:57 -0800

    Fix bugs with comment posts delte

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,10 @@ class CommentsController < ApplicationController
   def create
     params_id = "#{params[:commentable]}_id".downcase.to_sym
     parent = params[:commentable].constantize.find(params[params_id])
-    parent.comments.create(author_id: current_user.id, body: params[:comment_body])
-
-    User.send_notification(find_parent_id(parent), current_user.id, parent)
+    comment = parent.comments.create(author_id: current_user.id, body: params[:comment_body])
+    if comment.save
+      User.send_notification(find_parent_id(parent), current_user.id, parent)
+    end
     redirect_to :back
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,6 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   has_many :likes, as: :likeable, dependent: :destroy
 
+  validates :body, presence: true
   validates :author, presence: true
 end

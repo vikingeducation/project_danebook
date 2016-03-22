865328522a30bdd7eca00dbe9997ad01675aeebf
Julia Herron <julia.herron@gmail.com>
2016-03-22 14:24:23 -0700

    slip 'n slide comments

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -5,10 +5,8 @@ var expandComment = function() {
 
     var parentId = $(e.target).parent().data("pid")
     var itemToChange = '[data-pid=' + parentId + ']';
-
     $( itemToChange + ' .new-comment').toggle(800)
-    // need to target specific dataid + .new-comment
-    // $('.new-comment').show().slideDown(2000);
+
     if ($(itemToChange + ' .expand-comment').text() === "Hide") {
       $(itemToChange + ' .expand-comment').text("Comment")
     } else {

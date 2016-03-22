1ce83546b9d1fa22f35e6df39b5dad27b48ca984
Julia Herron <julia.herron@gmail.com>
2016-03-22 08:50:27 -0700

    basic js for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,22 +6,36 @@ class PostsController < ApplicationController
 
 
   def index
-    @posts = Post.where(user_id: params[:user_id]).order("created_at DESC")
+    @posts = Post.where(user_id: params[:user_id]).order(created_at: :desc)
     @user = User.find(params[:user_id])
     @new_post = current_user.posts.build if signed_in_user?
     @new_comment = Comment.new
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 
   def create
     @post = Post.new(whitelisted_params)
     @post.user_id = current_user.id
+
     if @post.save 
       flash[:success] = "Post created"
+      respond_to do |format|
+        format.html
+        format.js { render :create_success }
+      end
+
     else
       flash[:error] = "Unable to save post"
+      respond_to do |format|
+        format.html
+        format.js { render :new }
+      end
     end
-    redirect_to :back
   end
 
 

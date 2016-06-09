43b86ada66dc6cde77ffc8b2196423b8923d943e
cjvirtucio87 <cjv287@gmail.com>
2016-06-08 19:33:55 -0700

    Added controller methods for remembering user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
+      remember_check
       flash.now[:success] = 'Login successful.'
       redirect_to @user
     else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,5 +17,22 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
+  end
+
+  def remember_check(user)
+    remember_me = params[:session][:remember_me]
+    return remember user if remember_me == '1'
+    return forget user
+  end
 
 end

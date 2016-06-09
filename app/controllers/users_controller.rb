b9db2a677454b7814aac015f31c0790c5df692c7
cjvirtucio87 <cjv287@gmail.com>
2016-06-09 13:23:01 -0700

    Refactored about page. Link to edit action functional.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,8 +62,8 @@ class UsersController < ApplicationController
     end
 
     def correct_user
-      user = User.find_by(params[:id])
-      redirect_to root_url unless current_user user
+      user = User.find(params[:id])
+      redirect_to root_url unless current_user == user
     end
 
 end

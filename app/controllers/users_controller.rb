283360e1b38eb684980e9817c656526a5e757924
cjvirtucio87 <cjv287@gmail.com>
2016-06-09 15:07:48 -0700

    Edited pagination for users#index. 10 users per page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   def new

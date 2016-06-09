288ee72306aa7191d2e66f313119cf8351db9364
cjvirtucio87 <cjv287@gmail.com>
2016-06-09 12:20:32 -0700

    Added comments to sessions helper.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,18 +17,21 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
+  #Store remember_token in remember digest and store cookies.
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  #Set remember digest to nil and delete cookies.
   def forget(user)
     user.forget
     cookies.delete :user_id
     cookies.delete :remember_token
   end
 
+  #Remember or forget user depending on whether checkbox is checked.
   def remember_check(user)
     remember_me = params[:session][:remember_me]
     return remember user if remember_me == '1'

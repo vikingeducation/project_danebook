31390c2ad63a2f7d4c502a6c547279baffd7124e
cjvirtucio87 <cjv287@gmail.com>
2016-06-10 17:10:52 -0700

    Refactored fields for email, password, and password confirmation.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,20 @@
 module StaticPagesHelper
+
+  def to_field(attribute)
+    field = "#{attribute}_field"
+    if self.class.method_defined? field
+      return field
+    else
+      return "text_field"
+    end
+  end
+
+  def make_pages_form(obj,attribute)
+    content = obj.send to_field(attribute), 
+                          attribute,
+                          { class: 'form-control', 
+                          placeholder: attribute }
+    content_tag :div, content, class: 'form-group'
+  end
+
 end

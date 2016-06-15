53fe07ced0acd8b9c872f6d01759b23b45553764
cjvirtucio87 <cjv287@gmail.com>
2016-06-15 12:19:09 -0700

    Added confirmation_check method.

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,17 @@
 module StaticPagesHelper
 
+  #Check that returns attribute without '_confirmation'.
+  def confirmation_check(attribute)
+    if /confirmation/.match attribute.to_s
+      attribute_parts = attribute.to_s.split('_').map(&:to_sym)
+      return attribute_parts.first
+    else
+      attribute
+    end
+  end
+
   def to_field(attribute)
+    attribute = confirmation_check attribute
     field = "#{attribute}_field"
     if self.class.method_defined? field
       return field

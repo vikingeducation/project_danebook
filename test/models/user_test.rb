57816c923e1d5465263a8cd3d505bdc800636d38
cjvirtucio87 <cjv287@gmail.com>
2016-06-15 10:37:41 -0700

    Slight modification to search test.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,9 +2,11 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test 'should return users whose names contain \'Virtucio\'' do
-    users = User.search_by_full_name 'Virtucio'
+  test 'should match users by their first or last names' do
+    users = User.search_by_full_name 'virtucio'
     assert users.any? { |u| !!(/\AVirtucio\z/.match u.last_name) }
+    users = User.search_by_full_name 'tester'
+    assert users.any? { |u| !!(/\ATester\z/.match u.first_name) }
   end
 
 end

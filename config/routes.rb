671391cd86339542bd23c9b9e0b201b76b7dd529
Graham Turner <turnertgraham@gmail.com>
2016-08-10 14:28:47 -0400

    basics on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#home"
+  get "/timeline" => "static_pages#timeline"
+  get "/friends" => "static_pages#friends"
+  get "/about" => "static_pages#about"
+  get "/photos" => "static_pages#photos"
+  get "/about_edit" => "static_pages#about_edit"
 end

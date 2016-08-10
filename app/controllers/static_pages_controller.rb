70ba2cf98da1f682ba0acf4889efbf56ed04e8b4
“Matthew <matthew.hinea@gmail.com>
2016-08-10 12:14:05 -0700

    home and timeline pages now viewable

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+    
+  end
+
+  def timeline
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  root "static_pages#home"
+
+  get '/timeline', to: "static_pages#timeline"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

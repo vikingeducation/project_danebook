ff6512ac5a2f9230b568f19ef0d03f4bcf7868c4
Leo Ahnn <leoahnn@gmail.com>
2016-08-12 19:10:20 -0400

    can add friends

--- app/controllers/friendings_controller.rb
+++ app/controllers/friendings_controller.rb
@@ -1,9 +1,12 @@
 class FriendingsController < ApplicationController
 
   def create
-    session[:return_to] = request.referer
-    current_user.friended_users << User.find(params[:friend_id])
-    current_user.users_friended_by << User.find(params[:friend_id])
+    if current_user.friended_users.where(params[:friend_id]).empty?
+      session[:return_to] = request.referer
+      current_user.friended_users << User.find(params[:friend_id])
+      current_user.users_friended_by << User.find(params[:friend_id])
+    else
+      flash[:alert] = "already friends!"
     redirect_to session.delete(:return_to)
   end
 

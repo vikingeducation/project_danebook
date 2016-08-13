78bbabdbb7e355e7584b2e547ce327d39f7e45cd
David Jiang <davidjgrey@gmail>
2016-08-13 15:03:57 -0700

    you can comment on posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,10 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @user = @post.user
-    @comment = Comment.create(:post_id => params[:post_id], 
+    @comment = Comment.new(:post_id => params[:post_id].to_i, 
                               :user_id => current_user.id,
-                              :body => params[:body])
-    if @comment.save
+                              :body => params[:comment][:body])
+    if @comment.save!
       redirect_to user_posts_path(@user)
     else
       redirect_to user_posts_path(@user)

3172a90c8d396dd98de7bd3fb63342a73fc0e483
Philip Cole Johnson <philipcolejohnson@gmail.com>
2016-08-13 12:17:15 -0700

    Adds text for who likes a post

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -6,4 +6,34 @@ module LikesHelper
   def find_like(likable)
     likable.likes.find_by(user_id: current_user.id)
   end
+
+  def post_like_text(post)
+    remaining = post.likes.count
+    current_text = ""
+    current_likes = false
+    user = ""
+    extras = ""
+
+    if liked?(post)
+      current =  "You"
+      extras = " like this."
+      current_likes = true
+      remaining -= 1
+    else
+      current = ""
+    end
+
+    if remaining > 0
+      user = post.likes.where.not(user_id: current_user.id).first.user.first_name
+      user = " and " + user if current_likes
+      remaining -= 1
+      extras = current_likes ? " like this." : "likes this."
+    end
+
+    if remaining > 0
+      extras = " and #{remaining} others like this."
+    end
+
+    current + user + extras
+  end
 end

e63b8050a59528c5cc581e031dfdd6d6d2b8406a
cjvirtucio87 <cjv287@gmail.com>
2016-08-14 12:49:35 -0700

    Login fix.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,22 +4,17 @@ class SessionsController < ApplicationController
   skip_before_action :logged_in_user, except: [:destroy]
   skip_before_action :correct_user
   before_action :set_user, only: [:create]
-  before_action :activated_user, only: [:create]
+  before_action :validate_credentials, only: [:create]
 
   def new
   end
   
   def create
     @profile = @user.profile
-    if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      remember_check @user
-      flash[:success] = 'Login successful.'
-      redirect_to @user
-    else
-      flash[:danger] = 'Invalid username/password.'
-      redirect_to new_session_path
-    end
+    log_in @user
+    remember_check @user
+    flash[:success] = 'Login successful.'
+    redirect_to @user
   end
 
   def destroy
@@ -33,8 +28,11 @@ class SessionsController < ApplicationController
       @user = User.find_by(email: params[:session][:email])
     end
 
-    def activated_user
-      unless @user.activated?
+    def validate_credentials
+      if @user.nil? || !@user.authenticate(params[:session][:password])
+        flash[:danger] = 'Invalid username/password.'
+        redirect_to login_path
+      elsif !@user.activated?
         flash[:danger] = "Your account has not yet been activated. Please check your email before continuing."
         redirect_to root_url
       end

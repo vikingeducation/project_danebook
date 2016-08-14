49d5a7dfe99598f621977f493d6d366766ef50c0
Morgan Martin <morganm5201@gmail.com>
2016-08-14 09:10:06 -0400

    add comment display

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   get "login" => "sessions#new"
+  post 'login' => 'sessions#create'
   delete "logout" => "sessions#destroy"
 end
 

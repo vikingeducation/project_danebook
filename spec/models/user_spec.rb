d4ab0da0f52f63d213190712296b1f706d26c97d
cjvirtucio87 <cjv287@gmail.com>
2016-08-15 14:45:38 -0700

    Refactored User and Profile tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,71 +3,83 @@ require 'rails_helper'
 describe User do
   let(:user) { build(:user) }
 
-  it "validates the presence of an email address" do
-    should validate_presence_of(:email)
+  describe "credentials" do
+
+    it "validates the presence of an email address" do
+      should validate_presence_of(:email)
+    end
+
+    it "validates the uniqueness of an email address" do
+      should validate_uniqueness_of(:email)
+    end
+
+    it "validates the presence of a password" do
+      should validate_presence_of(:password)
+    end
+
+    it "accepts a password with a length that has at least 6 characters" do
+      should validate_length_of(:password).is_at_least(6)
+    end
+
+    it "accepts a password with a length that has at most 40 characters" do
+      should validate_length_of(:password).is_at_most(40)
+    end
+
+    it "has a secure password" do
+      is_expected.to have_secure_password
+    end
+
   end
 
-  it "validates the uniqueness of an email address" do
-    should validate_uniqueness_of(:email)
+  describe "associations" do
+
+    it "has a profile" do
+      expect(user).to have_one(:profile)
+    end
+
+    it "has friends" do
+      expect(user).to have_many(:friends)
+    end
+
+    it "belongs to a friendable" do
+      expect(user).to belong_to(:friendable)
+    end
+
+    it "belongs to a timeline" do
+      expect(user).to belong_to(:timeline)
+    end
+
+    it "has many posts" do
+      expect(user).to have_many(:posts)
+    end
+
+    it "has many comments" do
+      expect(user).to have_many(:comments)
+    end
+
+    it "has many likes" do
+      expect(user).to have_many(:likes)
+    end
+
   end
 
-  it "validates the presence of a password" do
-    should validate_presence_of(:password)
-  end
+  describe "name" do
+    
+    it "has a first name" do
+      expect(user).to respond_to(:first_name)
+    end
 
-  it "accepts a password with a length that has at least 6 characters" do
-    should validate_length_of(:password).is_at_least(6)
-  end
+    it "has a last name" do
+      expect(user).to respond_to(:last_name)
+    end
 
-  it "accepts a password with a length that has at most 40 characters" do
-    should validate_length_of(:password).is_at_most(40)
-  end
+    it "can show its full name" do
+      first_name = user.first_name
+      last_name = user.last_name
+      full_name = "#{first_name} #{last_name}"
+      expect(user.full_name).to eq(full_name)
+    end
 
-  it "has a secure password" do
-    is_expected.to have_secure_password
-  end
-
-  it "has a profile" do
-    expect(user).to have_one(:profile)
-  end
-
-  it "has friends" do
-    expect(user).to have_many(:friends)
-  end
-
-  it "belongs to a friendable" do
-    expect(user).to belong_to(:friendable)
-  end
-
-  it "belongs to a timeline" do
-    expect(user).to belong_to(:timeline)
-  end
-
-  it "has a first name" do
-    expect(user).to respond_to(:first_name)
-  end
-
-  it "has a last name" do
-    expect(user).to respond_to(:last_name)
-  end
-
-  it "can show its full name" do
-    first_name = user.first_name
-    last_name = user.last_name
-    full_name = "#{first_name} #{last_name}"
-    expect(user.full_name).to eq(full_name)
-  end
-
-  it "has many posts" do
-    expect(user).to have_many(:posts)
-  end
-
-  it "has many comments" do
-    expect(user).to have_many(:comments)
-  end
-
-  it "has many likes" do
-    expect(user).to have_many(:likes)
   end
 
   describe "#friend?" do

1f6bcd8b9f82af8c7a4abf846f0eb62b374e8901
Leo Ahnn <leoahnn@gmail.com>
2016-08-15 14:12:29 -0400

    fixed friend button

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,9 +9,9 @@ module UsersHelper
   def friending_link(user)
     unless current_user == user
       if current_user.friended_users.where(id: user.id).empty?
-        button_to "Friend #{user}", friendings_path(friend_id: user.id), method: :post
+        button_to "Friend #{user}", friendings_path(friend_id: user.id), method: :post, class: 'btn btn-primary btn-sm'
       else
-        button_to "Unfriend #{user}", friending_path(friend_id: user.id), method: :delete
+        button_to "Unfriend #{user}", friending_path(friend_id: user.id), method: :delete, class: 'btn btn-danger btn-sm'
       end
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.destroy_all
+Friending.destroy_all
+puts "tables cleaned out"
 
-
+puts "creating Users and posts"
 10.times do
   u = User.create!(email: Faker::Internet.email, password: "password", password_confirmation: "password")
   u.create_profile(birthday: Faker::Date.between(100.years.ago, 10.years.ago), first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, about: "People call me #{Faker::Superhero.name}")
@@ -17,16 +19,32 @@ User.destroy_all
   end
 end
 
-10.times do
+puts "creating posts likes and comments"
+
+20.times do
   u = User.all.sample
   post = Post.all.sample
   post.likes.create(user_id: u.id) if post.likes.where(user_id: u.id).empty?
   post.comments.create(user_id: u.id, body: Faker::Hipster.sentence)
 end
 
-10.times do
+puts "creating comment likes"
+20.times do
   u = User.all.sample
   comment = Comment.all.sample
   comment.likes.create(user_id: u.id) if comment.likes.where(user_id: u.id).empty?
-  comment.comments.create(user_id: u.id, body: Faker::Hipster.sentence)
+  # comment.comments.create(user_id: u.id, body: Faker::Hipster.sentence)
+end
+
+10.times do
+  u = User.all.sample
+  friend = User.all.sample
+  while friend == u
+    friend = User.all.sample
+  end
+  if u.friended_users.where(id: friend.id).empty?
+    puts "friending #{u} and #{friend}"
+    u.friended_users << friend
+    u.users_friended_by << friend
+  end
 end

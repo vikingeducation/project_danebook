ed4ca2540b195d29d3fa9acf87ffc6b3e69c7638
cjvirtucio87 <cjv287@gmail.com>
2016-08-17 12:18:51 -0700

    Finished users#edit tests.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 describe UsersController do
-  let!(:user) { create(:user) }
-  let!(:profile) { user.profile }
+  let!(:user) { build(:user) }
 
   describe '#new' do
 
@@ -26,33 +25,77 @@ describe UsersController do
 
   describe '#create' do
 
-    it 'assigns the user instance variable'
+    it 'assigns the user instance variable' do
+
+      post :create, user: { first_name: user.profile.first_name, last_name: user.profile.last_name, email: user.email, password: user.password }
+
+      expect(assigns(:user)).to be_a(User)
+
+    end
 
     context 'when the information is valid' do
 
-      it 'persists the user to the database'
+      before do
 
-      it 'persists the profile in the database'
+        post :create, user: { 
+          first_name: user.profile.first_name, 
+          last_name: user.profile.last_name, 
+          email: user.email, 
+          password: user.password,
+          password_confirmation: user.password }
 
-      it 'sends an activation email'
+      end
 
-      it 'informs the user about the activation email'
+      it 'persists the user to the database' do
+        
+        expect(assigns(:user).persisted?).to eq(true)
 
-      it 'redirects to the root url'
+      end
+
+      it 'persists the profile in the database' do
+
+        expect(assigns(:user).profile.persisted?).to eq(true)
+
+      end
+
+      it 'redirects to the root url' do
+
+        expect(response).to redirect_to(root_path)
+
+      end
 
     end
 
     context 'when the information is invalid' do
 
-      it 'does not persist the user to the database'
+      before do
 
-      it 'does not persist the user to the database'
+        post :create, user: { 
+          first_name: "", 
+          last_name: "", 
+          email: "", 
+          password: "",
+          password_confirmation: "" }
 
-      it 'sends an activation email'
+      end
 
-      it 'warns the user about the invalid information'
+      it 'does not persist the user to the database' do
 
-      it 'redirects to the signup page'
+        expect(assigns(:user).persisted?).to eq(false)
+
+      end
+
+      it 'does not persist the profile to the database' do
+      
+        expect(assigns(:user).profile).to be_nil
+
+      end
+
+      it 'redirects to the signup page' do
+
+        expect(response).to redirect_to(new_user_path)
+
+      end
 
     end
 
@@ -62,29 +105,98 @@ describe UsersController do
 
     context 'when the user is logged in and is the correct user' do
 
-      it 'sets the user, profile, city, state, and country instance variables'
+      before do
 
-      it 'renders the edit template'
+        user.save
+        session[:user_id] = user.id
+        get :edit, id: user.id
+
+      end
+
+      it 'sets the user, profile, city, state, and country instance variables' do
+
+        expect(assigns(:user)).to be_a(User)
+        expect(assigns(:profile)).to be_a(Profile)
+        expect(assigns(:cities)).to_not be_nil
+        expect(assigns(:states)).to_not be_nil
+        expect(assigns(:countries)).to_not be_nil
+
+      end
+
+      it 'renders the edit template' do
+
+        expect(response).to render_template(:edit)
+
+      end
 
     end
 
     context 'when the user is not logged in' do
 
-      it 'does not set the user, profile, city, state, and country instance variables'
+      before do
 
-      it 'tells the user to log in'
+        user.save
+        get :edit, id: user.id
 
-      it 'redirects the user to the login page'
+      end
+
+      it 'does not set the user, profile, city, state, and country instance variables' do
+
+        expect(assigns(:user)).to be_nil
+        expect(assigns(:profile)).to be_nil
+        expect(assigns(:cities)).to be_nil
+        expect(assigns(:states)).to be_nil
+        expect(assigns(:countries)).to be_nil
+
+      end
+
+      it 'tells the user to log in' do
+
+        expect(flash[:notice]).to eq "You must first log in."
+
+      end
+
+      it 'redirects the user to the login page' do
+
+        expect(response).to redirect_to(login_path)
+
+      end
 
     end
 
     context 'when the user is not the correct user' do
 
-      it 'does not set the user, profile, city, state, and country instance variables'
+      before do
 
-      it 'tells the user that he is not authorized to access this page'
+        user.save
+        new_user = create(:user)
+        session[:user_id] = new_user.id
+        controller.stub(:current_user) { new_user }
+        get :edit, id: user.id
 
-      it 'redirects the user to the root path'
+      end
+
+      it 'does not set the user, profile, city, state, and country instance variables' do
+
+        expect(assigns(:user)).to be_nil
+        expect(assigns(:profile)).to be_nil
+        expect(assigns(:cities)).to be_nil
+        expect(assigns(:states)).to be_nil
+        expect(assigns(:countries)).to be_nil
+
+      end
+
+      it 'tells the user that he is not authorized to access this page' do
+
+        expect(flash[:notice]).to eq("You are not authorized to access that page.")
+
+      end
+
+      it 'redirects the user to the root path' do
+
+        expect(response).to redirect_to(root_path)
+
+      end
 
     end
 

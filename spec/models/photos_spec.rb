f2eb5bbe4bd4aae8d0c3ad769d0d10e1f9188d97
cjvirtucio87 <cjv287@gmail.com>
2016-08-18 14:10:49 -0700

    Added some tests.

---
+++ spec/models/photos_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe Photo do
+
+
+  describe "data" do
+
+    it "should have a data attribute"
+
+  end
+
+  describe "filename" do
+
+    it "should have a filename attribute"
+
+  end
+
+  describe "mime_type" do
+
+    it "should have a mime_type attribute"
+
+  end
+
+  describe "#photo_data=" do
+
+    it "should set the data attribute of the row"
+
+    it "should set the filename attribute of the row"
+
+    it "should set the mime_type attribute of the row"
+
+  end
+
+
+end

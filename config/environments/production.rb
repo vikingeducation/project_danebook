a7cbc9dbf4925142f53e06fa2bb15322ab39aa27
Leo Ahnn <leoahnn@gmail.com>
2016-08-19 13:45:38 -0400

    adjust production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,15 +80,12 @@ Rails.application.configure do
   config.paperclip_defaults = {
 
     # Don't forget to make S3 your storage option!
-    :storage => :s3,
-    :s3_region => ENV['AWS_REGION'],
-    :s3_credentials => {
-      :s3_host_name => "s3-us-west-2.amazonaws.com",
-      :bucket => Rails.application.secrets.s3_bucket_name,
-      :access_key_id => Rails.application.secrets.aws_access_key_id,
-      :secret_access_key => Rails.application.secrets.aws_secret_access_key
+    storage: :s3,
+    s3_region: ENV['AWS_REGION'],
+    s3_credentials: {
+      bucket: Rails.application.secrets.s3_bucket_name,
+      access_key_id: Rails.application.secrets.aws_access_key_id,
+      secret_access_key: Rails.application.secrets.aws_secret_access_key
     }
   }
-
-
 end

d0b8ea47fbf95ed379360bf1884df0f40400b724
cjvirtucio87 <cjv287@gmail.com>
2016-08-18 20:01:09 -0700

    Friends on timeline fixed.

--- app/helpers/timelines_helper.rb
+++ app/helpers/timelines_helper.rb
@@ -2,4 +2,8 @@ module TimelinesHelper
   include UsersHelper
   include PhotosHelper
 
+  def render_friend_box(friend)
+    render partial: 'static_pages/friend_box', locals: { friend: friend }
+  end
+
 end

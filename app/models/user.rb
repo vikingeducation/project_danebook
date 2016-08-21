972990e6ccadb9781f1060ee66ee0558a0dfdbd1
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-21 03:31:40 -0700

    added email and the configuration details as needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
 
   before_create :generate_token
   after_create :create_profile
+  after_create { send_welcome_email(self.id) }
 
   has_secure_password
 
@@ -70,4 +71,12 @@ class User < ApplicationRecord
   def photo_count
     self.photo_posts.count
   end
+
+  private
+
+    def send_welcome_email(id)
+      user = User.find_by_id(id)
+      UserMailer.welcome(user).deliver!
+    end
+    # handle_asynchronously :send_welcome_email
 end

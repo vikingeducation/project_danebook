4e36797feef974c86b2ce080e4b93e751de902d4
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-21 02:23:59 -0700

    added sessions controller and its actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+  skip_before_action :require_login, :only => [:new, :create]
+
+  def new
+    if current_user
+      flash[:success] = "Welcome back #{current_user.first_name}"
+      redirect_to root_path
+    else
+      @user = User.new
+    end
+  end
+
+  def create
+    @user = User.find_by_email(params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      if params[:remember_me]
+        permanent_sign_in(@user)
+      else
+        sign_in(@user)
+      end
+      flash[:success] = "You've successfully signed in"
+      redirect_to root_url
+    else
+      flash.now[:error] = "We couldn't sign you in"
+      @user = User.new
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:success] = "You've successfully signed out"
+    redirect_to login_path
+  end
+end

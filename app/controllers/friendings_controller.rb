ed61e6c8816f667eaf780996afac5b2121e54643
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-21 04:35:07 -0700

    fixes for view logic bugs

--- app/controllers/friendings_controller.rb
+++ app/controllers/friendings_controller.rb
@@ -6,14 +6,13 @@ class FriendingsController < ApplicationController
 
   def create
     if signed_in_user?
-      session[:return_to] = request.referer
       friending_recipient = User.find(params[:id])
       if current_user.friended_users << friending_recipient
         flash[:success] = "Successfully friended #{friending_recipient.first_name}"
       else
         flash[:error] = "Failed to friend! Uh ohhh"
       end
-      redirect_to session.delete(:return_to)
+      redirect_to :back
     else
       redirect_to login_path
     end
@@ -21,14 +20,13 @@ class FriendingsController < ApplicationController
 
   def destroy
     if signed_in_user?
-      session[:return_to] = request.referer
       unfriended_user = User.find(params[:id])
       if current_user.friended_users.delete(unfriended_user)
         flash[:success] = "Successfully unfriended #{unfriended_user.first_name}"
       else
         flash[:error] = "Failed to unfriend. . . "
       end
-      redirect_to session.delete(:return_to)
+      redirect_to :back
     else
       redirect_to login_path
     end
--- app/models/posting.rb
+++ app/models/posting.rb
@@ -3,17 +3,19 @@ class Posting < ApplicationRecord
   belongs_to :user
 
   def self.current_user_activities(user)
+    ids = user.friended_user_ids + [user.id]
     self.joins("LEFT OUTER JOIN posts ON postings.postable_id = posts.id").
     joins("LEFT OUTER JOIN photos ON postings.postable_id = photos.id").
-    where(user_id: user.id).
+    where(user_id: [ids]).
     order(created_at: :desc)
   end
 
   def self.friend_activities(user)
-    user_friend_ids = user.friended_users.pluck(:id)
+    # user_friend_ids = user.friended_users.pluck(:id)
+    ids = user.friended_user_ids + [user.id]
     self.joins("LEFT OUTER JOIN posts ON postings.postable_id = posts.id").
     joins("LEFT OUTER JOIN photos ON postings.postable_id = photos.id").
-    where(user_id: [user_friend_ids]).
+    where(user_id: [ids]).
     order(created_at: :desc)
   end
 end

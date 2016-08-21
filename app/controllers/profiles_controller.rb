393c36ab6393cbe4969256aba1e9bbfeb6fd9a2a
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-21 02:22:09 -0700

    added profile controller and associated actions

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,40 @@
+class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:edit, :update]
+  before_action :require_login, :except => [:show]
+
+  def show
+    @user = User.find(params[:user_id])
+  end
+
+  def edit
+    if signed_in_user?
+      @user = current_user
+    else
+      redirect_to login_path
+    end
+  end
+
+  def update
+    if signed_in_user? && current_user.id == params[:id]
+      if @profile.update(white_listed_profile_params)
+        # flash[:success] = "Your profile has been updated!"
+        redirect_to user_profile_path(current_user)
+      else
+        # flash.now[:error] = "Uhhh oh something went wrong trying to update your profile"
+        render :edit
+      end
+    else
+      redirect_to login_path
+    end
+  end
+
+  private
+
+    def set_profile
+      @profile = current_user.profile
+    end
+
+    def white_listed_profile_params
+      params.require(:profile).permit(:college, :hometown, :currently_lives, :telephone, :words_to_live_by, :about_me)
+    end
+end

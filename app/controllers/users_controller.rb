98112a576df8949e42563d75869bc1a5c6cc7808
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-21 02:48:23 -0700

    added views and assets for each controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, :except => [:new, :create, :index]
+  before_action :set_user, :except => [:new, :create, :index, :newsfeed]
   before_action :require_login, :except => [:index, :new, :create, :show]
   before_action :require_current_user, :only => [:edit, :update, :destroy]
 
@@ -47,6 +47,16 @@ end
 def destroy
 end
 
+def newsfeed
+  if signed_in_user?
+    @user = current_user
+    @post = Post.new
+    @feeds = feed_posts(@user)
+  else
+    flash[:error] = "Please login to complete this operation"
+    redirect_to login_path
+  end
+end
 
   private
 
@@ -73,4 +83,17 @@ end
         redirect_to root_path
       end
     end
+
+    def feed_posts(user)
+      feed = Posting.activities(user).map do |posting|
+        resource = find_instance(posting)
+        [User.find(posting.user_id), resource]
+      end
+    end
+
+    def find_instance(posting)
+      type = posting.postable_type.classify
+      type.constantize.find(posting.postable_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#newsfeed'
+
+  resource :session, :only => [:new, :create, :destroy]
+  get "/login" => "sessions#new"
+  get "/logout" => "sessions#destroy"
+  delete "/logout" => "sessions#destroy"
+  get "/login" => "users#new"
+
+  resources :users, only: [:index, :show, :create, :update] do
+    resource :profile, only: [:show]
+    resources :photos, only: [:new, :index, :edit, :show]
+    resources :friendings, only: [:index]
+    get 'newsfeed' => "users#newsfeed"
+  end
+
+  resource :profile, only: [:edit, :update]
+
+  resources :photos, only: [:create, :update, :destroy] do
+    resources :likes, only: [:create], :defaults => { :likeable => 'Photo'}
+    resources :comments, only: [:new, :create]
+  end
+
+  resources :posts, only: [:create, :update, :destroy] do
+    resources :likes, only: [:create], :defaults => { :likeable => 'Post'}
+    resources :comments, only: [:new, :create]
+  end
+
+  resources :comments, only: [:create, :destroy] do
+    resources :likes, only: [:create], :defaults => { :likeable => 'Comment'}
+  end
+
+  resources :likes, only: [:destroy]
+
+  resources :friendings, :only => [:create, :destroy]
 end

d9cdbf6906f4ac19d9ed9ad9aef5b16bfe71e2bd
jsteenb2 <jonathansteenbergen@gmail.com>
2016-08-21 02:11:48 -0700

    added likes controller and associated actions

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,30 @@
+class LikesController < ApplicationController
+  def create
+    if signed_in_user?
+      type = params[:likeable].classify
+      resource = type.constantize.find(params["#{type.downcase}_id"])
+      if resource.likes.create(:user_id => current_user.id)
+        flash[:success] = "Like contributed to the post!"
+      else
+        flash[:error] = "Couldn't establish the like"
+      end
+      redirect_to :back
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    if signed_in_user?
+      @like = Like.find(params[:id])
+      if @like.destroy
+        flash[:success] = "Unliked the post"
+      else
+        flash[:error] = "Couldn't make the unlike"
+      end
+      redirect_to :back
+    else
+      redirect_to login_path
+    end
+  end
+end

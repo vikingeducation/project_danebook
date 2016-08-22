f342c6e3482fab648ab4b7e2c0154ab3c8dfe5c4
David Jiang <davidjgrey@gmail>
2016-08-22 15:57:11 -0700

    Aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,12 +7,13 @@ Rails.application.configure do
   #S3 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_region => Rails.application.secrets.s3_region
     :s3_credentials => {
     :s3_host_name => "s3-us-west-2.amazonaws.com",
     :bucket => Rails.application.secrets.s3_bucket_name,
     :access_key_id => Rails.application.secrets.aws_access_key_id,
     :secret_access_key => Rails.application.secrets.aws_secret_access_key,
-    :s3_region=> Rails.application.secrets.s3_region
+    #:s3_region=> Rails.application.secrets.s3_region
     }
   }
 

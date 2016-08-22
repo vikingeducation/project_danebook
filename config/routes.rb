aff507ed56171b0a15ec65747712992c6e667116
Dylan Lynch <lynchd2@tcnj.edu>
2016-08-22 18:27:57 -0400

    add redirect for bad URLs

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   get "login" => "sessions#new"
   delete "logout" => "sessions#destroy"
 
-  get '*path' => redirect("http://nouveller.com/404/")
+  get '*path' => redirect("/")
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

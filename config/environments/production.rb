9c4b8890312ef2c060ca6a4279206289d611cbc6
Leo Ahnn <leoahnn@gmail.com>
2016-08-22 13:35:14 -0400

    configured sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,8 +95,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      =>Rails.application.secrets.sendgrid_username,
+      :password       => Rails.application.secrets.sendgrid_password,
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
     }

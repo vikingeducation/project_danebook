143e16731c42ba891db454bdd1473158b31b1783
Deepak <grc.deepak@gmail.com>
2016-08-22 22:55:31 -0700

    part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get 'timeline', to: 'static_pages#timeline', as: 'timeline'
+  get 'friends', to: 'static_pages#friends', as: 'friends'
+  get 'about', to: 'static_pages#about', as: 'about'
+  get 'photos', to: 'static_pages#photos', as: 'photos'
 end

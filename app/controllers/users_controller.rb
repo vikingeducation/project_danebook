bceffec5421f1f69e6f0822b7f7dd8c61eae9999
BranLiang <lby89757@hotmail.com>
2016-09-02 22:03:23 +0800

    signup donw

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(white_list_params)
+    if @user.save
+      flash[:success] = ["Sign Up success!"]
+      redirect_to signup_path
+    else
+      flash[:danger] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+
+  private
+    def white_list_params
+      params.require(:user).permit(:email,
+                                   :password,
+                                   :password_confirmation)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   before_create :generate_token
-  
+  has_secure_password
+
+  validates :password, :length => { :in => 8..24 },
+                       :allow_nil => true
 end

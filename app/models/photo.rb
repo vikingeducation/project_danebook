ed0611e65e8fc6f770af982a3d6c4a4c99fd3830
BranLiang <lby89757@hotmail.com>
2016-09-10 13:08:48 +0800

    modify the new photo style

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ApplicationRecord
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/style/missing.jpeg"
+  has_attached_file :picture, styles: { medium: "600x600>", thumb: "300x300>" }, default_url: "/images/style/missing.jpeg"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
   belongs_to :user
 

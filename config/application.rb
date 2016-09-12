127d43f2340cd35eeeb03190db6d22bf84704e54
BranLiang <lby89757@hotmail.com>
2016-09-12 10:31:24 +0800

    modify the home page

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module ProjectDanebookV2
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('lib')
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"  
   end
 end

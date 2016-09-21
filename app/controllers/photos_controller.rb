bdff5b71a1d3b2924413f57c8838665efe315f80
Adrian Mui <adrian.mui@gmail.com>
2016-09-21 02:07:34 -0500

    touchup, photo working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PhotosController < ApplicationController
 
   # create a belongs_to  cover, profile photo for profile. and add migration
@@ -7,27 +8,21 @@ class PhotosController < ApplicationController
   end
 
   def create
-    which_id = params["photoable"].downcase + "_id"
-    if (which_id == "user_id")
+    which = params["photoable"].downcase
+    if (which == "user")
       @photoable = params["photoable"].classify.constantize.find(params["user_id"]);
-      @photo = @photoable.photos.build(white_listed_photo_params)
+      @photo = @photoable.photos.build(whitelisted_photo_params)
       
-    elsif (which_id == "profile_id")
-      picture = Photo.find(params[:photo_id]).dup
-      attributes = picture.attributes.select do |attr, value|
-        Photo.column_names.include?(attr.to_s) end
+    elsif (which == "profile")
+      @photoable = params["photoable"].classify.constantize.find_by_user_id(params["user_id"])
+      avatar = Photo.find(params[:photo_id]).avatar
+      @photo = @photoable.build_photo()
+      @photo.avatar = avatar;
+    end  
 
-      
-      @photoable = params["photoable"].classify.constantize.find_by_user_id(params["user_id"]);
-
-      @photo = @photoable.build_photo(attributes);
-
-
-    end
-    
     if @photo.save
-      flash[:success] = "Photo was uploaded"
-      redirect_to current_user
+        flash[:success] = "Photo was uploaded"
+        redirect_to current_user
     else
       flash[:error] = "Photo did not load"
       render :index
@@ -46,7 +41,7 @@ class PhotosController < ApplicationController
 
   private
 
-    def white_listed_photo_params
+    def whitelisted_photo_params
       params.require(:photo).
         permit(:label, :avatar)
     end

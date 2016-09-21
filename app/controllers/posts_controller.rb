acb726fc3dfa93e7158735cdda95759c1c6c5306
Leo Ahnn <leo.ahnn@gmail.com>
2016-09-21 12:29:13 -0400

    working posts ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,20 +2,34 @@ class PostsController < ApplicationController
 
   def create
     session[:return_to] = request.referer
-    current_user.posts.build(post_params)
-    if current_user.save
-      flash[:success] = "Created New Post"
-    else
-      flash[:error] = "Could Not Create Post"
+    @post = current_user.posts.build(post_params)
+    respond_to do |format|
+      if current_user.save
+        flash[:success] = "Created New Post"
+        format.html { redirect_to session.delete(:return_to) }
+        format.js { render :create }
+      else
+        flash[:error] = "Could Not Create Post"
+        format.html { redirect_to session.delete(:return_to) }
+        format.js { head :none }
+      end
     end
-    redirect_to session.delete(:return_to)
   end
 
   def destroy
     session[:return_to] = request.referer
-    post = current_user.posts.find(params[:id])
-    post.delete ? flash[:success] = "Deleted Post" : flash[:alert] = "Did Not Delete Post"
-    redirect_to session.delete(:return_to)
+    @post = current_user.posts.find(params[:id])
+    respond_to do |format|
+      if @post.destroy
+        flash[:success] = "Deleted Post"
+        format.html { redirect_to session.delete(:return_to) }
+        format.js { render :destroy }
+      else
+        flash[:error] = "Could Not Delete Post"
+        format.html { redirect_to session.delete(:return_to) }
+        format.js { head :none }
+      end
+    end
   end
 
 

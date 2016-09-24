87a6441f77efca7f372dd2e61fcaec06569dd3df
BranLiang <lby89757@hotmail.com>
2016-09-24 14:47:49 +0800

    add the comment function

---
+++ app/assets/javascripts/photos.js
@@ -0,0 +1,24 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+// var MY_APP = MY_APP || {};
+//
+// MY_APP.photoCtr = (function () {
+// 	var stub = {};
+//
+// 	stub.init = function () {
+// 		newPhotoLinkListener();
+// 	};
+//
+// 	var newPhotoLinkListener = function () {
+//
+// 	};
+//
+// 	return stub;
+// })();
+//
+// document.addEventListener("turbolinks:load", function () {
+// 	if ($("body").data("controller") === 'photos') {
+// 		MY_APP.photoCtr.init();
+// 	};
+// })

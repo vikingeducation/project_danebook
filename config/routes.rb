ba2327bb59a0e08b6a52e9cd6856fadb97f9b51a
Tamal Riedman <triedman99@gmail.com>
2016-10-06 14:00:30 -0700

    About_edit Page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'friends', to: 'static_pages#friends', as: 'friends'
   get 'about', to: 'static_pages#about', as: 'about'
   get 'photos', to: 'static_pages#photos', as: 'photos'
+  get 'about_edit', to: 'static_pages#about_edit', as: 'about_edit'
 end

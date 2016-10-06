f8d5d2e2e4852fcf5b817acaae24008f5732f9ae
Tamal Riedman <triedman99@gmail.com>
2016-10-06 13:31:05 -0700

    Friends Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get 'timeline', to: 'static_pages#timeline', as: 'timeline'
+  get 'friends', to: 'static_pages#friends', as: 'friends'
 end

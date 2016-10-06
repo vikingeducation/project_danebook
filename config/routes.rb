441c5e3e05bc04673e63e9caf546f5b5d99b3f21
Tamal Riedman <triedman99@gmail.com>
2016-10-06 13:38:03 -0700

    Photos Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'timeline', to: 'static_pages#timeline', as: 'timeline'
   get 'friends', to: 'static_pages#friends', as: 'friends'
   get 'about', to: 'static_pages#about', as: 'about'
+  get 'photos', to: 'static_pages#photos', as: 'photos'
 end

e57ac4e33b11faabb9185ac47c8feeddb5ec0913
Tamal Riedman <triedman99@gmail.com>
2016-10-06 13:34:01 -0700

    About Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'timeline', to: 'static_pages#timeline', as: 'timeline'
   get 'friends', to: 'static_pages#friends', as: 'friends'
+  get 'about', to: 'static_pages#about', as: 'about'
 end

f1ba0f21b6a83d17f81df97d481e31e44cfcc173
Deepak <grc.deepak@gmail.com>
2016-10-13 19:35:37 -0700

    user model tested

--- Gemfile
+++ Gemfile
@@ -53,6 +53,18 @@ group :production do
   gem 'rails_12factor'
 end
 
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'factory_girl_rails', '~> 4.0'
+  gem 'guard-rspec', require: false
+end
+
+group :test do
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'capybara'
+  gem 'launchy'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console'
---
+++ spec/factories.rb
@@ -0,0 +1,26 @@
+# spec/factories.rb
+FactoryGirl.define do
+  # A block defining the attributes of a model
+  # The symbol is how you will later call it
+  # Factory Girl assumes that your class name
+  # is the same as the symbol you passed
+  # (so here, it assumes this is a User)
+  factory :user do
+    first_name  "Foo"
+    last_name   "Bar"
+    email       "foo@bar.com"
+    password    "foobar123"
+    birthday    "#{DateTime.now}"
+    gender_cd   1
+  end
+
+  factory :profile do
+    college "College1"
+    hometown "543 San Fran, CA"
+    address  "123 West field, Kentucky"
+    phone    "123-456-708"
+    status   "win ein win"
+    about    "hahahahaha"
+    user
+  end
+end

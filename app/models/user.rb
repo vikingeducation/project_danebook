70722fc69e40b6f4b0248f571e63324b79160d62
James Harris <computersherpa@gmail.com>
2016-12-07 17:18:11 -0600

    User Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+
+  def generate_token
+    begin
+      self[:auth_token] = SecureRandom.urlsafe_base64
+    end while User.exists?(:auth_token => self[:auth_token])
+  end
+
+  def regenerate_auth_token
+    self.auth_token = nil
+    generate_token
+    save!
+  end
+
 end

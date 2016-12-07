347095508f5d9a276ec4628b4a75e6a487561b10
James Harris <computersherpa@gmail.com>
2016-12-07 17:33:06 -0600

    5:33 PM

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users do
+    resource :profile, on: :member, except: [:new, :create, :destroy]
+  end
 end

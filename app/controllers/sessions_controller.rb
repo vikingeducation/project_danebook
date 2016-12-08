78a9be536d75c0ca2e435f8f309d0d01379a47b6
luke-schleicher <luke.j.schleicher@gmail.com>
2016-12-07 18:38:35 -0600

    adding cookie-based auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,22 @@
 class SessionsController < ApplicationController
+
   def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      if params[:remember_me]
+        permanent_sign_in(@user)
+      else
+        sign_in(@user)
+      end
+      flash[:success] = "Welcome!"
+      redirect_to @user
+    else
+      flash.now[:error] = "Couldn't log in"
+      render :new
+    end
   end
 
   def destroy
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,24 @@
 class User < ApplicationRecord
 
+  before_create :generate_token
+
   has_secure_password
 
   validates :password,
             length: { minimum: 6 },
             allow_nil: true
+
+
+  def generate_token
+    begin
+      self[:auth_token] = SecureRandom.urlsafe_base64
+    end while User.exists?(:auth_token => self[:auth_token])
+  end
+
+  def regenerate_auth_token
+    self.auth_token = nil
+    generate_token
+    save!
+  end
             
 end

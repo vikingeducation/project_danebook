be4b061e709b114c8bd4cbeafd17ec90856ae57b
Deepak <grc.deepak@gmail.com>
2016-12-08 23:15:09 -0800

    organizing css code

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  get 'timeline', to: 'static_pages#timeline', as: 'timeline'
-  get 'friends', to: 'static_pages#friends', as: 'friends'
-  get 'about', to: 'static_pages#about', as: 'about'
-  get 'photos', to: 'static_pages#photos', as: 'photos'
-
   root :to => 'users#new'
+
   resources :users, only: [:new, :create, :show, :index] do
     get 'about', on: :member
     get 'timeline', on: :member
     get 'friends', on: :member
     get 'photos', on: :member
   end
+
   resources :friendships, only: [:create, :destroy]
   resources :profiles, only: [:edit, :update]
+
   resources :posts, only: [:create, :update, :destroy] do
     get 'like', on: :member
     get 'unlike', on: :member
@@ -21,6 +19,7 @@ Rails.application.routes.draw do
       get 'unlike', on: :member
     end
   end
+
   resources :photos, only: [:new, :create, :show, :destroy, :update] do
     resources :comments, only: [:create, :destroy] do
       get 'like', on: :member

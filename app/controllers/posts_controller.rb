e465e0d2a7a2a16dd02c3d895dbe28250d5b78e2
James Harris <computersherpa@gmail.com>
2016-12-09 17:10:05 -0600

    Save posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,31 @@
 class PostsController < ApplicationController
 
   def index
-    @profile = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+    @post = current_user.posts.build
   end
 
   def show
 
   end
+
+  def create
+    @post = current_user.posts.build(strong_post_params)
+    if @post.save
+      flash[:success] = "Post published!"
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:warning] = @post.errors.full_messages
+      @profile = User.find(params[:user_id]).profile
+      render :index
+    end
+  end
+
+  private
+    def strong_post_params
+      p params
+      params.require(:post).permit(:body, :user_id)
+    end
 
 end

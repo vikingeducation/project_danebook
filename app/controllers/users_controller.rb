1551c7bc94854eda25336c52c37ac9152c31f215
James Harris <computersherpa@gmail.com>
2016-12-08 19:08:32 -0600

    Birthday validation, styles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,17 @@ class UsersController < ApplicationController
     p params
     @user = User.new(strong_user_params)
     if @user.save
-      flash[:success] = "Welcome to Danebook!"
+      flash[:success] = "Welcome to Danebook, #{@user.first_name}!"
       sign_in(@user)
       redirect_to edit_user_profile_path(@user)
     else
       flash[:warning] = @user.errors.full_messages
       render :new
     end
+  end
+
+  def index
+    redirect_to root_path
   end
 
   private
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,4 @@
 class Profile < ApplicationRecord
   belongs_to :user, inverse_of: :profile
+  validates :birthday, inclusion: { in: (100.years.ago..1.day.ago), message: "must have been born within the last 100 years"}
 end

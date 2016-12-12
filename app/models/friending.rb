9d134c43c02eb677909c5722da3d8f2519827b8c
James Harris <computersherpa@gmail.com>
2016-12-11 19:59:31 -0600

    Display friends

---
+++ app/models/friending.rb
@@ -0,0 +1,8 @@
+class Friending < ApplicationRecord
+
+  belongs_to :friend_initiator, foreign_key: :friender_id, class_name: "User"
+  belongs_to :friend_recipient, foreign_key: :friend_id, class_name: "User"
+
+  validates :friend_id, uniqueness: { scope: :friender_id }
+
+end

a50c85031f46ab378d3832ce18702956dc1bf388
James Harris <computersherpa@gmail.com>
2016-12-14 20:02:40 -0600

    View test

--- spec/controllers/like_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-describe LikesController do
-
-  
-
-end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :post do
     association :user
     body { "Body of post" }
+    created_at { Time.now }
   end
 end

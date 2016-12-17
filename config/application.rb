b91a0544857b0ffda29f637f69802f5ba6bc79d5
James Harris <computersherpa@gmail.com>
2016-12-16 18:28:17 -0600

    Alternate default profile image URL

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Danebook
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :delayed_job
+    config.serve_static_assets = true
   end
 end

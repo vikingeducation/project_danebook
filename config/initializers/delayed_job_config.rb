30f47b4185364b654b787b1f0c7c74a8445852cb
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-19 12:31:29 -0700

    log level for worker

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -7,4 +7,4 @@ Delayed::Worker.default_queue_name = 'default'
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
-Delayed::Worker.logger.level = :warn
+Delayed::Worker.logger.level = 1

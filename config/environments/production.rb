72713473fa5c3b943c738c23fb1e9ca63ba73eea
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-19 12:21:58 -0700

    ar logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
+  ActiveRecord::Base.logger.level = 1
 
   config.paperclip_defaults = {
 

6d9fb8acef621a4b26d99e5e6e7c4c2d44e4c564
Kenneth Kottenstette <kennykotten@gmail.com>
2016-12-19 07:42:10 -0800

    validation tweak

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :user, inverse_of: :profile
   validates :first_name, :last_name, presence: true
-
-
-  validates :motto, :about, length: { maximum: 600 }
   validates :gender, presence: true
-  validates :phone, length: { maximum: 10 }, :format => /[0-9]/
+
 
 
 end

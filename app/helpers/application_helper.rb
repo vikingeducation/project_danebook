6e01746e0ac021b4299332745fccb73b782e1625
Francisco Suero <suerofrancisco@yahoo.com>
2016-12-19 17:04:03 -0500

    fixing for danebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
      if user.cover_photo
        user.cover_photo.expiring_url(3600, :original)
      else
-       "/assets/hogwarts_small.jpg"
+       "https://s3.amazonaws.com/danebook191919/hogwarts_small.jpg"
      end
    end
 
@@ -11,7 +11,7 @@ module ApplicationHelper
      if user.profile_photo
        user.profile_photo.expiring_url(3600, :cover)
      else
-       "/assets/user_silhouette.png"
+       "https://s3.amazonaws.com/danebook191919/user_silhouette.png"
      end
    end
 

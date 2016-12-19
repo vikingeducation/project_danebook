86bb3c377d44faa3700aefee3d633229b9458fa7
Sampson Crowley <sampsonsprojects@gmail.com>
2016-12-19 11:55:44 -0700

    fix post rendering

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,9 +7,7 @@ module UsersHelper
 
   def render_posts(posts)
     if posts.length > 0
-      posts.each do |post|
-        render partial: 'shared/posts/post', locals: { user: post.user, post: post, size: "small"}
-      end
+      render partial: 'shared/posts/post', collection: posts, as: :post, locals: { size: "small"}
     else
       '<br><h2 class="text-center">No posts to show</h2>'.html_safe
     end

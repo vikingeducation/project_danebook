c8179703a9cbe7693244beb4247f58d0f817e07d
Dexter Ford <dexter.ford.77@gmail.com>
2016-12-19 18:16:42 -0600

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ puts "Users created."
 puts "Building Candlejack..."
 
 user = User.create(first_name: "Candlejack", last_name: "Martin", email: "cj@aol.com", password: "password")
-user.profile = Profile.new( birthday: Time.now, gender: ["Male", "Female", "IDK", "Other"].sample, college: "Buttface University", hometown: "Uncool Place USA", city: "Radlands Bumble USA", telephone: "555-EAT-GLUE", words_to_live_by: "ONCE UPON A HAT THERE WAS ME AND YEAH", about_me: "IDK BRO" )
+user.profile = Profile.new( birthday: Time.now, gender: ["Male", "Female", "IDK", "Other"].sample, college: "Buttface University", hometown: "Uncool Place USA", city: "Radlands Bumble USA", telephone: "555-EAT-GLUE", words_to_live_by: "watch where you going ya fool", about_me: "WHEN IN THE COORS OF HUMID INVENTS FOR SCOURGE AND ELEVEN BEERS AGO WE THE PAINFUL OF THE BLUEFRIGHTENED GATES OF HYSTERIA DO DECLAREAR THIS HERE SOUP TO BEAN INDEPENDUNCE DRE FOR DOCTOR DADA OF INTELLIGENT QUALIFIED REPRESENTATIVES BEING UNDERKNEETH TWENTY TELEVISUALS GUSHING RAMPANT BONAFIDE BONAPARTES OF BORN BONES TORN SLOWLY APART OF ME OWNLY SOUL LONELY ONLY TOLL BLOWN ROLLING UNKNOWNINGLY SLOWLY LONELY FORLORNLY UP THE LONG LOWLY LANES OF SNAKING LAKES IN DUCKS OF DRAKES OF QUACKING BLACK TRACKS LACKING GRISTLED HEATHER IN HOOTS IN HEATS IN VELDTS IN MELTS OF PELTS IN GRUELY SUNHOLY TETHERS WEATHER OR NOT YOU LIKE IT" )
 
 puts "Candlejack created."
 puts "Building posts..."

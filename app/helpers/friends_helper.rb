fb4071e9fec07cd3d0c4bc113d9b547ea1016076
tt <ttwang.303@gmail.com>
2017-02-09 12:58:57 -0500

    friend button formatting

--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -2,9 +2,9 @@ module FriendsHelper
 
   def friend_button(user)
     if current_user.friends?(user.id)
-      link_to 'Remove as Friend', user_friend_path(id: current_user.id), method: "DELETE", class: 'btn btn-large btn-primary friend-button'
+      link_to 'Remove as Friend', user_friend_path(id: current_user.id), method: "DELETE", class: 'btn btn-large btn-info friend-button'
     else
-      link_to 'Add as Friend', user_friends_path(user_id: user.id), :method => :post, class: 'btn btn-large btn-primary friend-button'
+      link_to 'Add as Friend', user_friends_path(user_id: user.id), :method => :post, class: 'btn btn-large btn-info friend-button'
     end
   end
 

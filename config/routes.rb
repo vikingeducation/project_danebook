facf70a487bd6ba1219810274c1f80bc7d4c5edb
yxlau <yixuan.lau@gmail.com>
2017-02-10 20:46:19 +0800

    Set up timeline page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#home'
-  get 'static_pages', to: 'static_pages#home', as: 'static_page'
+  get 'home', to: 'static_pages#home', as: 'home'
+  get 'timeline', to: 'static_pages#timeline', as: 'timeline'
 end

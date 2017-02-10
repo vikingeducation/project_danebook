9a248d441d769f7b5431a1a8a98c9a1ad43f0491
yxlau <yixuan.lau@gmail.com>
2017-02-10 21:05:08 +0800

    Refactor HTML

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,4 +13,7 @@ class StaticPagesController < ApplicationController
 
   def photos
   end
+
+  def about_edit
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'timeline', to: 'static_pages#timeline', as: 'timeline'
   get 'friends', to: 'static_pages#friends', as: 'friends'
   get 'photos', to: 'static_pages#photos', as: 'photos'
-  get 'about', to: 'static_ages#about', as: 'about'
+  get 'about', to: 'static_pages#about', as: 'about'
+  get 'about_edit', to: 'static_pages#about_edit', as: 'about_edit'
 end

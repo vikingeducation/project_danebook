7df8713ee9f0b6fde292645d5129627f84ae8e15
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-02-10 18:19:00 +0800

    Set up controller for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#home'
+  get 'static_pages', to: 'static_pages#home', as: 'static_page'
 end

8437ed1ba1ea8dc92d3a5c92b5204a4a2b83a1fa
Visiona <darbis@gmail.com>
2017-03-17 20:53:33 +0000

    Created roots, actions and remplates for timeline, friends and about websites

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,17 @@ class StaticPagesController < ApplicationController
   def home
 
   end
-  
+
+  def timeline
+    
+  end
+
+  def friends
+
+  end
+
+  def about
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
+  get 'home' => 'static_pages#home'
+  get 'timeline' => 'static_pages#timeline'
+  get 'friends' => 'static_pages#friends'
+  get 'about' => 'static_pages#about'
+
+  # resources :static_pages, :only => [:home, :timeline]
 end

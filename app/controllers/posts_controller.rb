e5765b1b84d34f5afc33a69da0eedef3ac89ca80
Visiona <darbis@gmail.com>
2017-07-01 15:05:12 +0100

    Finished Posting epic on Timeline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :require_current_user => [:create, :destroy]
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.order('created_at DESC')
@@ -10,9 +12,21 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
     if @post.save!
       flash[:success] = "You have created new post"
-      redirect_to [@user, @post]
+      redirect_to user_timeline_path
     else
       flash[:danger] = "Something went wrong! No post created!"
+      render :index
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:success] = "You have deleted post successfully!"
+      redirect_to user_timeline_path
+    else
+      flash.now[:danger] = "Deleting post didn't work :("
       render :index
     end
   end
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,6 @@
 module ProfilesHelper
+
+  def full_name(post)
+    "#{post.user.profile.first_name} #{post.user.profile.last_name}"
+  end
 end

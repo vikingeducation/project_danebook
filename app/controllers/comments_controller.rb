da22cff64de5e4ec44cdda46d07daccaf498e515
Visiona <darbis@gmail.com>
2017-07-02 21:48:51 +0100

    Fixed issue in comment forms, added delete button to currenty logged user comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,39 @@
 class CommentsController < ApplicationController
 
+  before_action :require_comment_creator, :only => [:destroy]
+
   def create
     @comment = current_user.comments.build(comment_params)
     if @comment.save!
       flash[:success] = "You have created new comment"
-      redirect_to user_timeline_path
+      redirect_to user_timeline_path(@comment.user_id)
     else
       flash[:danger] = "Something went wrong! No post created!"
       redirect_to user_timeline_path
     end
   end
 
-  # def destroy
-  #   @post = Post.find(params[:post_id])
-  #   @user = current_user
-  #   if @post.destroy
-  #     flash[:success] = "You have deleted post successfully!"
-  #     redirect_to user_timeline_path
-  #   else
-  #     flash.now[:danger] = "Deleting post didn't work :("
-  #     render :index
-  #   end
-  # end
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      flash[:success] = "You have deleted comment successfully!"
+      redirect_to user_timeline_path(current_user.id)
+    else
+      flash.now[:danger] = "Deleting comment didn't work :("
+      redirect_to :back
+    end
+  end
 
   private
   def comment_params
     params.require(:comment).permit(:body, :commentable_id, :commentable_type)
+  end
+
+  def require_comment_creator
+    unless params[:user_id] == @current_user.id.to_s
+      flash[:danger] = "Your are not authorized to do this."
+      redirect_to root_url
+    end
   end
 
 end

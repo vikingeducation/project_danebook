fce3ef4d9df802a396d1fc854e7f49a324ba43f6
Visiona <darbis@gmail.com>
2017-07-03 23:36:30 +0100

    Finished info about how many likes have a certain comment

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,20 +1,9 @@
 module LikesHelper
 
   def first_few_likes(post)
-    # three_likes =
     post.likes.order('created_at').limit(3)
-    # three_names = []
-    # three_likes.each {|l| l.user_id == current_user.id ? (three_names << "You") : (three_names << full_name(l) ) }
-    # three_names
-    # three_likes.map {|l| l.user }
-    # .pluck(:user_id)
-    # ids.map {|id| User.find(id) }
   end
 
-
-  def three_ids_who_liked(post)
-    post.likes.order('created_at').limit(3).pluck(:user_id)
-  end
 
   def number_of_likes(post)
     post.likes.count

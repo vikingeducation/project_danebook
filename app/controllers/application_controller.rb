4293d5f7414734185163120f7535bde6b94ebc39
Visiona <darbis@gmail.com>
2017-07-04 22:42:02 +0100

    Removed displaying post window in timeline if current user is not the owner of the timeline

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < ActionController::Base
   end
 
   def require_current_user
-    unless params[:id] == @current_user.id.to_s
+    unless params[:id] == @current_user.id.to_s || params[:user_id] == @current_user.id.to_s
       flash[:danger] = "Your are not authorized to do this."
       redirect_to root_url
     end

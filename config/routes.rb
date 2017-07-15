43c00e71d88742ba045a815ad104b7db2d585e44
andie5 <andieasa@googlemail.com>
2017-07-15 23:16:48 +0100

    Added all danebook pages and refactored navabar and toolbar in partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
-  resources :static_pages
+  resources :static_pages, :only => [:index]
+
+  get "/static_pages/timeline", to: "static_pages#timeline", as: "timeline"
 end

5846866ed92b16e5925dcb34fca39770d1c40597
Visiona <darbis@gmail.com>
2017-07-26 21:19:32 +0100

    Fixed subnavbar highlights on current page; working on friending contorller

--- app/controllers/friendings_controller.rb
+++ app/controllers/friendings_controller.rb
@@ -1,9 +1,8 @@
 class FriendingsController < ApplicationController
 
   def create
-    recipient = User.find(params[:friending][:friender_id])
-    @friending = current_user.friended_users.build(friending_params)
-    if @friending.save
+    recipient = User.find(params[:user_id])
+    if current_user.friended_users << recipient
       flash[:success] = "You have created new friend"
       redirect_to :back
     else
@@ -13,27 +12,14 @@ class FriendingsController < ApplicationController
   end
 
   def destroy
-    @friending = find_friending(friending_params, friending_params_inverse)
-    if @friending.destroy
+    removed_friend = User.find(params[:user_id])
+    if current_user.friended_users.delete(removed_friend)
       flash[:success] = "You have lost a friend successfully!"
       redirect_to :back
     else
       flash.now[:danger] = "Loosing friend didn't work"
       redirect_to :back
     end
-  end
-
-  private
-  def friending_params
-    params.require(:friending).permit(:friend_id, :friender_id)
-  end
-
-  def friending_params_inverse
-    params.require(:friending).permit(:friend_id, :friender_id)
-    a = params[:friending][:friend_id]
-    b = params[:friending][:friender_id]
-    params[:friending][:friend_id] = b
-    params[:friending][:friender_id] = a
   end
 
 end
--- app/helpers/friends_helper.rb
+++ app/helpers/friends_helper.rb
@@ -1,9 +1,3 @@
 module FriendsHelper
 
-  def current_user_friended?(current_user_id, user_id)
-    u = Friending.all.where(:friender_id => [current_user.id, user_id])
-
-    u.any? {|u| [current_user.id, user_id].include? u.friend_id }
-  end
-
 end

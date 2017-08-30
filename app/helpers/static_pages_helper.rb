aea32e6368c3d6e0e2e8ec75811478d6b3214d6a
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-08-30 16:51:36 -0600

    fixed navbar for home(not logged in) vs. other pages(logged in)

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,8 @@
 module StaticPagesHelper
+
+  # would need to change hard coded link when make more dynamic
+  def determine_nav
+    current_page?('http://localhost:3000/home') ? 'shared/navbar' : 'shared/loggedin_navbar'
+  end
+
 end

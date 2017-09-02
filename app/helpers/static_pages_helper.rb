89d082229ff6052a4c44dff4ad910f4f4c77f3c1
Mariah Schneeberger <mariah.acacia@gmail.com>
2017-09-02 12:53:21 -0600

    fixed navbar issue

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -2,7 +2,7 @@ module StaticPagesHelper
 
   # would need to change hard coded link when make more dynamic
   def determine_nav
-    current_page?(home_path) ? 'shared/navbar' : 'shared/loggedin_navbar'
+    current_page?(root_path) ? 'shared/navbar' : 'shared/loggedin_navbar'
   end
 
 end

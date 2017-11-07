6364bb0e462c177f50a4c5537371c4afa3c392d0
andie5 <andieasa@googlemail.com>
2017-11-07 22:24:56 +0000

    added links to about pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in(@user) 
+      permanent_sign_in(@user)
       flash[:success] = "Created new user!"
       redirect_to edit_user_path(@user)
     else
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      flash[:success] = "Update user details!"
+      flash[:success] = "Updated user details!"
       redirect_to user_path(current_user.id)
     else
       flash.now[:error] = "Failed to update user details!"

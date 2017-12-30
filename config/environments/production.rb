03c59754eab4e93b211301ee618061df17dff27f
andie5 <andieasa@googlemail.com>
2017-12-30 22:41:08 +0000

    added missing config line in the production config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
 
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
   # Don't forget to make S3 your storage option!
   :storage => :s3,
 

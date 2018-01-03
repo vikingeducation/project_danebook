3e0ab1bd52a9485ec99fd331400a49006388b1e9
anne <richardson.ae@gmail.com>
2018-01-03 13:08:54 -0600

    Adds flashes with bootsrap styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def bootstrap_flash_class(type)
+    case type
+    when 'alert' then 'warning'
+    when 'error' then 'danger'
+    when 'notice' then 'success'
+    else
+      'info'
+    end
+  end
+
 end

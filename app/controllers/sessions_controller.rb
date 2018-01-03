65c19ae33ddb0ab93842e61f26f83017fbb195df
anne <richardson.ae@gmail.com>
2018-01-03 13:12:28 -0600

    Adds sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  skip_before_action :require_login, :only => [:new, :create]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      sign_in(@user)
+      flash[:success] = "You've successfully signed in"
+      redirect_to root_url
+    else
+      flash.now[:error] = "We couldn't sign you in"
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    flash[:success] = "You've successfully signed out"
+    redirect_to root_url
+  end
+end

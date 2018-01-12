daf0059da4bbda2f7dc424172639391ea2ccc279
anne <richardson.ae@gmail.com>
2018-01-12 13:21:09 -0600

    Refactors posts for brevity

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -5,11 +5,10 @@ class Feed
   def initialize(user_id:)
     @user = User.find(user_id)
     @post = @user.authored_posts.new
-    @friends = @user.friends
   end
 
   def posts
-    @posts ||= Post.where(user: @friends).order(created_at: :DESC)
+    @posts ||= Post.where(user: @user.friends).order(created_at: :DESC)
   end
 
 end

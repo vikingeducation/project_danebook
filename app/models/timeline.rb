28bf9f3e8bfd22dc512e2ce8d2ca1580574391a6
anne <richardson.ae@gmail.com>
2018-01-12 13:20:44 -0600

    Adds placeholders for photos

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,12 +1,13 @@
 class Timeline
 
-  attr_reader :user, :posts, :post
+  attr_reader :user, :posts, :post, :friends #, :photos
 
   def initialize(user_id:)
     @user = User.find(user_id)
     @posts = @user.authored_posts.order(created_at: :DESC)
     @post = @user.authored_posts.new
-    @friends = @user.friended_users
+    @friends = @user.friends
+    # @photos = @user.photos
   end
 
 end

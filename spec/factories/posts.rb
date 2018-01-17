87e375af1c19deff5256a372d7a274b07bc5d66e
anne <richardson.ae@gmail.com>
2018-01-17 14:02:29 -0600

    Finishes model specs for users

---
+++ spec/factories/posts.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+
+  factory :post do
+    body "this is the post body"
+    user
+  end #post
+
+end #FactoryBot
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user){ build(:user) }
+  let(:password_min){ 8 }
+  let(:password_max){ 24 }
 
-  describe "attributes" do
+  describe "attributes & validations" do
     it "is valid with default attributes" do
       expect(user).to be_valid
     end
@@ -39,6 +41,22 @@ RSpec.describe User, type: :model do
     it "returns a user's name as a string" do
       expect(user.name.is_a?(String)).to be == true
     end
+
+    it "with a password that's in the length range is valid" do
+      user_1 = build(:user, password: generate_string(password_min))
+      user_2 = build(:user, password: generate_string(password_max))
+      expect([user_1, user_2]).to all(be_valid)
+    end
+
+    it "with a password that's too short is invalid" do
+      user = build(:user, password: generate_string(password_min - 1))
+      expect(user).not_to be_valid
+    end
+
+    it "with a password that's too long is invalid" do
+      user = build(:user, password: generate_string(password_max + 1))
+      expect(user).not_to be_valid
+    end
   end #attributes
 
   describe "Authorship associations" do
@@ -59,6 +77,37 @@ RSpec.describe User, type: :model do
       expect(user).to respond_to(:posts)
       expect(user).to respond_to(:authored_posts)
     end
+
+    it "responds to the comments association" do
+      expect(user).to respond_to(:comments)
+      expect(user).to respond_to(:authored_comments)
+    end
+
+    it "responds to the likes association" do
+      expect(user).to respond_to(:likes)
+      expect(user).to respond_to(:posts_they_like)
+      expect(user).to respond_to(:comments_they_like)
+    end
+  end
+
+  describe "Initiated Friendship Associations" do
+    it "responds to the initiated_friendings association" do
+      expect(user).to respond_to(:initiated_friendings)
+    end
+
+    it "responds to the friended_users association" do
+      expect(user).to respond_to(:friended_users)
+    end
+  end
+
+  describe "Recieved Friendship Associations" do
+    it "responds to the received_friendings association" do
+      expect(user).to respond_to(:received_friendings)
+    end
+
+    it "responds to the users_friended_by association" do
+      expect(user).to respond_to(:users_friended_by)
+    end
   end
 
   describe "polymorphic commenting associations" do
@@ -75,18 +124,42 @@ RSpec.describe User, type: :model do
 
   end
 
-  describe "#post_count" do
+  describe "instance methods" do
 
-    let(:num_posts){ 3 }
+    describe "#display_name" do
+      it "displays the user's name if a name is present" do
+        expect(user.display_name).to eq(user.name)
+      end
 
-    before do
-      user.posts = create_list(:post, num_posts)
-      user.save!
+      it "displays the user's email if there is no name" do
+        user.name = nil
+        expect(user.display_name).to eq(user.email)
+      end
     end
 
-    it "returns the count of a User's posts" do
-      expect(user.post_count).to eq(num_posts)
+    describe "#friends" do
+      let(:user_1){ build(:user) }
+      let(:user_2){ build(:user) }
+
+      it 'lists all friends' do
+        user.friended_users = [user_1]
+        user.users_friended_by = [user_1, user_2]
+        expect(user.friends.count).to eq(2)
+      end
     end
-  end # post_count
+
+    describe "#post_count" do
+      let(:num_posts){ 3 }
+
+      before do
+        user.posts = create_list(:post, num_posts)
+        user.save!
+      end
+
+      it "returns the count of a User's posts" do
+        expect(user.post_count).to eq(num_posts)
+      end
+    end # post_count
+  end #instance methods
 
 end

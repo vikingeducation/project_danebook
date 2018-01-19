1e51db07eb6398c77ee5eb3e88321cf7b94314ad
anne <richardson.ae@gmail.com>
2018-01-18 18:40:24 -0600

    Adds feed specs for comments

--- spec/features/commenting_spec.rb
+++ spec/features/commenting_spec.rb
@@ -6,14 +6,6 @@ RSpec.feature 'Commenting', type: :feature do
     visit root_path
   end
 
-  context "A visitor" do
-
-    xscenario 'can not make a new comment' do
-      visit new_user_post_path(user)
-      expect(current_path).to eq(login_path)
-    end
-  end #visitors
-
   context 'As a signed-in user' do
     before do
       login(user)
@@ -47,6 +39,51 @@ RSpec.feature 'Commenting', type: :feature do
     context 'on their Feed' do
       before do
         visit user_feed_path(user)
+      end
+
+      scenario "can comment on their own feed post" do
+        write_post
+        comment_body = 'sample comment'
+        fill_in('comment[body]', match: :first, with: comment_body)
+        click_button('Comment')
+        expect(page).to have_content(comment_body)
+      end
+
+      scenario "can delete their comment from their feed post" do
+        write_post
+        comment_body = 'sample comment'
+        fill_in('comment[body]', match: :first, with: comment_body)
+        click_button('Comment')
+        comment = Comment.last
+        click_link('Delete Comment')
+        expect(page).to_not have_content("comments/#{comment.id}")
+      end
+
+      scenario "can comment on their friend's feed post" do
+        user_2 = create(:user)
+        user_2_post = create(:post, user_id: user_2.id, body: "user_2's post")
+        create(:friending, recipient_id: user_2.id, initiator_id: user.id)
+        visit user_feed_path(user)
+
+        comment_body = 'sample comment'
+        fill_in('comment[body]', match: :first, with: comment_body)
+        click_button('Comment')
+        expect(page).to have_content(comment_body)
+      end
+
+      scenario "can delete their comment from their friend's feed post" do
+        user_2 = create(:user)
+        user_2_post = create(:post, user_id: user_2.id, body: "user_2's post")
+        create(:friending, recipient_id: user_2.id, initiator_id: user.id)
+        visit user_feed_path(user)
+
+        comment_body = 'sample comment'
+        fill_in('comment[body]', match: :first, with: comment_body)
+        click_button('Comment')
+
+        comment = Comment.last
+        click_link('Delete Comment')
+        expect(page).to_not have_content("comments/#{comment.id}")
       end
     end #Feed
 

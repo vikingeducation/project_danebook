2e2e26cb9c11ecb2254a17e9940a898457c447cc
anne <richardson.ae@gmail.com>
2018-01-18 18:25:32 -0600

    Adds feed posting specs

--- spec/features/posting_spec.rb
+++ spec/features/posting_spec.rb
@@ -48,7 +48,7 @@ RSpec.feature 'Posting', type: :feature do
         expect(page).to_not have_content("posts/#{post_1.id}")
       end
 
-      scenario "can't see other's posts on my own timeline" do
+      scenario "see only posts by me on my own timeline" do
         user_2 = create(:user)
         user_2_post = create(:post, user_id: user_2.id, body: 'user_2 post')
         visit user_timeline_path(user)
@@ -61,6 +61,39 @@ RSpec.feature 'Posting', type: :feature do
     context 'on their Feed' do
       before do
         visit user_feed_path(user)
+      end
+
+      scenario 'can post on my own feed' do
+        post_body = 'This is a sample post'
+        fill_in('post[body]', with: post_body)
+        within('div.post-form') do
+          click_button('Post')
+        end
+        expect(page).to have_content(post_body)
+      end
+
+      scenario 'can see my posts on my own feed' do
+        post_1 = create(:post, user_id: user.id, body: 'post 1')
+        post_2 = create(:post, user_id: user.id, body: 'post 2')
+        visit user_feed_path(user)
+        expect(page).to have_content(post_1.body)
+        expect(page).to have_content(post_2.body)
+      end
+
+      scenario 'can delete a post from my own feed' do
+        post_1 = create(:post, user_id: user.id, body: 'post 1')
+        visit user_feed_path(user)
+        click_link('Delete Post')
+        expect(page).to_not have_content("posts/#{post_1.id}")
+      end
+
+      scenario "can see my friends' posts in my feed" do
+        user_2 = create(:user)
+        user_2_post = create(:post, user_id: user_2.id, body: "user_2's post")
+        create(:friending, recipient_id: user_2.id, initiator_id: user.id)
+
+        visit user_feed_path(user)
+        expect(page).to have_content(user_2_post.body)
       end
     end #Feed
 

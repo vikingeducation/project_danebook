ceab3db92521074d7f66560637af737e41cf9683
anne <richardson.ae@gmail.com>
2018-01-26 13:11:35 -0600

    Resolves unfriending bug so users can unfriend a friend regardless of how the friendship was formed

--- app/controllers/friendings_controller.rb
+++ app/controllers/friendings_controller.rb
@@ -4,20 +4,23 @@ class FriendingsController < ApplicationController
     friending_recipient = User.find(params[:id])
     if current_user.friended_users << friending_recipient
       flash[:success] = "Successfully friended #{friending_recipient.name}"
-      redirect_to user_timeline_path(friending_recipient)
+      redirect_to session.delete(:return_to)
     else
       flash[:error] = "Failed to friend!"
-      redirect_to user_timeline_path(friending_recipient)
+      redirect_to session.delete(:return_to)
     end
   end
 
   def destroy
     unfriended_user = User.find(params[:id])
-
-    current_user.friended_users.delete(unfriended_user)
+    if current_user.friended_users.include?(unfriended_user)
+      current_user.friended_users.delete(unfriended_user)
+    elsif current_user.users_friended_by.include?(unfriended_user)
+      current_user.users_friended_by.delete(unfriended_user)
+    else
+    end
     flash[:success] = "Successfully unfriended"
-    redirect_to user_timeline_path(unfriended_user)
-
+    redirect_to session.delete(:return_to)
   end
 
 end

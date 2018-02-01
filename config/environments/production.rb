0fd1363b9c8955e0c090b873c5a5fb0760b749e8
anne <richardson.ae@gmail.com>
2018-02-01 16:56:43 -0600

    Sendgrid integration debugging 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   }
   config.action_mailer.delivery_method ||= :smtp
 
-  config.action_mailer.default_url_options = { host: ENV['SENDGRID_HOST_URL'] }
+  config.action_mailer.default_url_options = { host: 'https://lortzdanebook.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
